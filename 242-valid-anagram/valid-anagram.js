4de58d03628c0595b8e7f424bcb3638521c1078b
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2024-07-09 22:53:33 +0530

    Time: 88 ms (14.58%) | Memory: 53.7 MB (37.65%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,33 +4,10 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let map_s = {}
-    let map_t = {}
-    for(let i=0; i<s.length; i++){
-        char = s[i]
-        if(map_s[char]){
-            map_s[char]++
-        }
-        else{
-            map_s[char] = 1
-        }
-    }
-    for(let i=0; i<t.length; i++){
-        char = t[i]
-        if(map_t[char]){
-            map_t[char]++
-        }
-        else{
-            map_t[char] = 1
-        }
-    }
-    if(Object.keys(map_s).length !== Object.keys(map_t).length){
-        return false
-    }
-    for(let key in map_s){
-        if(map_s[key] !== map_t[key]){
-            return false
-        }
-    }
-    return true
+    let char_s = s.split("").sort().join("")
+
+    let char_t = t.split("").sort().join("")
+
+    return char_s == char_t ? true : false
+    
 };
