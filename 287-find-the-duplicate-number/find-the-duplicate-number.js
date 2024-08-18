305012d30722ddf278db8e642b210fa6ea496298
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2024-08-18 13:35:26 +0530

    Time: 78 ms (65.08%) | Memory: 71.7 MB (17.54%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let set = new Set();
+        for(let i=0;i<nums.length;i++) {
+            if(set.has(nums[i])) return nums[i];
+            set.add(nums[i]);
+        }
+        return -1;
+};
