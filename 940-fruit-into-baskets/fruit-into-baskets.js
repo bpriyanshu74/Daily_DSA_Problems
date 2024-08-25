a7c9388f44a9eecc14f03f4697debeb165496111
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2024-08-26 03:49:42 +0530

    Time: 108 ms (51.85%) | Memory: 62 MB (57.75%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function(fruits) {
+    let l=0,r=0,maxl=0,map=new Map()
+    let n = fruits.length
+    while(r<n){
+        if(map.has(fruits[r])){
+            map.set(fruits[r], map.get(fruits[r])+1)
+        }
+        else{
+            map.set(fruits[r],1)
+        }
+        if(map.size > 2){
+            while(map.size > 2){
+                map.set(fruits[l], map.get(fruits[l]) - 1)
+                if(map.get(fruits[l]) == 0) map.delete(fruits[l])
+                l++
+            }
+        }
+        if(map.size <= 2){
+            maxl = Math.max(maxl, r-l+1)
+        }
+        r++
+    }
+    return maxl
+    
+};
