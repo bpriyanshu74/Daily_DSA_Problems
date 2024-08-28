36e1ddee5c5e41898cfad3413aca97498dcbe071
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2024-08-28 20:59:46 +0530

    Time: 86 ms (35.45%) | Memory: 56.8 MB (10.49%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -5,21 +5,23 @@
  */
 var characterReplacement = function(s, k) {
     let maxl = 0
-    for(let i=0; i< s.length; i++){
-        let hash = new Array(26).fill(0)
-        let maxf = 0
-        for(let j=i; j<s.length; j++){
-            let index = s[j].charCodeAt(0) - 'A'.charCodeAt(0)
-            hash[index]++
-            maxf = Math.max(maxf, hash[index])
-            let change = (j-i+1) - maxf
-            if(change <= k){
-                maxl = Math.max(maxl, j-i+1)
+    let hash = new Array(26).fill(0)
+    let left = 0, right = 0, maxf = 0, n = s.length, change = 0
+    while(right < n){
+        let index = s[right].charCodeAt(0) - 'A'.charCodeAt(0)
+        hash[index]++
+        maxf = Math.max(maxf, hash[index])
+        while(right-left+1 - maxf > k){
+            let indexl = s[left].charCodeAt(0) - 'A'.charCodeAt(0)
+            hash[indexl]--
+            let maxf = 0
+            for(let ele of hash){
+                maxf = Math.max(maxf, ele)
             }
-            else{
-                break
-            }
+            left++
         }
+        maxl = Math.max(right-left+1, maxl) 
+        right++   
     }
     return maxl
 };
