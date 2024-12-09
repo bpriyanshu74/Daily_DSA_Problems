91e7f74f6cc85894a4234687aed7aead25d133d3
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2024-12-09 13:33:55 +0530

    Time: 140 ms (14.09%) | Memory: 57.7 MB (5.31%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -4,20 +4,22 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-    let maxl = 0
-    let hash = new Array(26).fill(0)
-    let left = 0, right = 0, maxf = 0, n = s.length, change = 0
-    while(right < n){
-        let index = s[right].charCodeAt(0) - 'A'.charCodeAt(0)
-        hash[index]++
-        maxf = Math.max(maxf, hash[index])
-        if(right-left+1 - maxf > k){
-            let indexl = s[left].charCodeAt(0) - 'A'.charCodeAt(0)
-            hash[indexl]--
+    let left = 0, right = 0, maxl = 0, map = new Map(), maxf = 0, curwindow = 0
+    while(right < s.length){
+        map.set(s[right], map.get(s[right])+1 || 1)
+        maxf = Math.max(...map.values())
+        
+
+        while((right-left+1) - maxf > k){
+            map.set(s[left], map.get(s[left])-1)
             left++
+            maxf = Math.max(...map.values())
         }
-        maxl = Math.max(right-left+1, maxl) 
-        right++   
+
+        maxl = Math.max(right-left+1, maxl)
+        right++
     }
+
     return maxl
+    
 };
