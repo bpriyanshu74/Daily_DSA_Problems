dc4e41533d75b62b17b8d6b9bb4450ccc9643644
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2024-12-27 18:47:43 +0530

    Time: 0 ms (100.00%) | Memory: 49.4 MB (33.66%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -4,6 +4,22 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
+
+    function bsearch(arr, target){
+        let low = 0, high = arr.length - 1
+        while(low <= high){
+            let mid = Math.floor((low +high) / 2)
+
+            if(target == arr[mid]) return true
+            if(target < arr[mid]){
+                high = mid - 1
+            }
+            if(target > arr[mid]){
+                low = mid+1
+            }
+        }
+        return false
+    } 
     let rows = matrix.length
     let cols = matrix[0].length
 
@@ -12,9 +28,7 @@ var searchMatrix = function(matrix, target) {
         let high = matrix[i][cols-1]
 
         if(target >= low && target <= high){
-            for(let j=0; j< cols; j++){
-                if(matrix[i][j] == target) return true
-            }
+            return bsearch(matrix[i], target)
         }
     }
     return false
