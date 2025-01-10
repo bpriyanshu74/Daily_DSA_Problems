cbe573628292c7ad521b11c06187ef14601ebb5c
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-01-10 18:35:33 +0530

    Time: 398 ms (5.08%) | Memory: 51.8 MB (17.59%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,17 +4,30 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let temp = []
-    for(let i= 0; i<nums.length; i++){
-        let req = target - nums[i]
+
+    let n = nums.length
+    let temparr = new Array(n)
+
+    function findIndex(arr, key){
+        for(let i=0; i< arr.length; i++){
+            if(arr[i] == key) return i
+        }
+        return -1
         
-        
-        if(temp.includes(req)){
-            return [i,temp.indexOf(req)]
+    }
+
+    for(let i=0 ;i< n ;i++){
+        let diff = target - nums[i]
+        let key = findIndex(temparr, diff)
+
+        if(key == -1){
+            temparr[i] = nums[i]
         }
         else{
-            temp[i] = nums[i]
+            return [i,key]
         }
+
+
     }
     
 };
