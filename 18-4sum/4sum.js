7bf5fa4440609cd850479b5bf753ede3c9686b33
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-01-11 17:23:32 +0530

    Time: 17 ms (85.57%) | Memory: 54.5 MB (24.51%) - LeetSync

--- 18-4sum/4sum.js
+++ 18-4sum/4sum.js
@@ -4,38 +4,31 @@
  * @return {number[][]}
  */
 var fourSum = function(nums, target) {
-    nums.sort((a, b) => a - b);
-  const quadruplets = [];
-  const n = nums.length;
-  for (let i = 0; i < n - 3; i++) {
-    if (i > 0 && nums[i] === nums[i - 1]) {
-      continue;
-    }
-    for (let j = i + 1; j < n - 2; j++) {
-      if (j > i + 1 && nums[j] === nums[j - 1]) {
-        continue;
-      }
-      let left = j + 1;
-      let right = n - 1;
-      while (left < right) {
-        const sum = BigInt(nums[i]) + BigInt(nums[j]) + BigInt(nums[left]) + BigInt(nums[right]);
-        if (sum < target) {
-          left++;
-        } else if (sum > target) {
-          right--;
-        } else {
-          quadruplets.push([nums[i], nums[j], nums[left], nums[right]]);
-          while (left < right && nums[left] === nums[left + 1]) {
-            left++;
-          }
-          while (left < right && nums[right] === nums[right - 1]) {
-            right--;
-          }
-          left++;
-          right--;
+    nums.sort((a,b) => a-b)
+    let res = []
+
+    for(let i=0; i< nums.length; i++){
+        if(i>0 && nums[i] == nums[i-1]) continue
+        for(let j=i+1; j<nums.length; j++){
+            if(j != (i+1) && nums[j] == nums[j-1]) continue
+            let k=j+1, l=nums.length - 1
+            
+            while(k<l){
+                let sum = nums[i] + nums[j] + nums[k] + nums[l]
+                if(sum == target){
+                    let temp = [nums[i], nums[j], nums[k], nums[l]]
+                    res.push(temp)
+                    while(k<l && nums[k] == nums[k+1]) k++
+                    while(k<l && nums[l] == nums[l-1]) l--
+                    k++
+                    l--
+                }
+                else if(sum > target) l--
+                else k++
+            }
+            
         }
-      }
     }
-  }
-  return quadruplets;
+
+    return res
 };
