39cd06a06a3eac5f58423402c2b2d885551e2f9e
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-01-17 22:50:18 +0530

    Time: 4 ms (43.30%) | Memory: 53.2 MB (83.96%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -3,23 +3,39 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    // brute force
-    let rows = new Set()
-    let cols = new Set()
-
-    for(let i=0; i< matrix.length; i++){
-        for(let j=0; j< matrix[0].length; j++){
+    let col0 = 1
+    for(let i=0; i<matrix.length; i++){
+        for(let j=0; j<matrix[0].length; j++){
             if(matrix[i][j] == 0){
-                rows.add(i)
-                cols.add(j)
+                matrix[i][0] = 0
+                if(j!= 0){
+                    matrix[0][j] = 0
+                }
+                else{
+                    col0 = 0
+                }
             }
         }
     }
-    for(let i=0; i< matrix.length; i++){
-        for(let j=0; j<matrix[0].length; j++){
-            if(rows.has(i) || cols.has(j)){
-                matrix[i][j] = 0
+
+    for(let i=1; i<matrix.length; i++){
+        for(let j=1; j< matrix[0].length; j++){
+            if(matrix[i][j] != 0){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0
+                }
             }
+        }
+    }
+
+    if(matrix[0][0] == 0){
+        for(j=0; j < matrix[0].length; j++){
+            matrix[0][j] = 0
+        }
+    }
+    if(col0 == 0){
+        for(let i=0; i<matrix.length; i++){
+            matrix[i][0] = 0
         }
     }
     
