fb079bc349567a91f1d57734c1bd4d3e48d610a7
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-03-21 10:23:51 +0530

    Time: 1 ms (61.81%) | Memory: 59.7 MB (12.38%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.js
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -13,14 +13,18 @@ var trap = function(height) {
     let lmaxm = height[0]
 
     for(let i=1; i<n; i++){
-        lmaxm = Math.max(lmaxm, height[i])
+        if(height[i] > lmaxm){
+            lmaxm = height[i]
+        }
         leftbound[i] = lmaxm
     }
 
     let rmaxm = height[n-1]
 
     for(let i=n-2; i>= 0; i--){
-        rmaxm = Math.max(rmaxm, height[i])
+        if(height[i] > rmaxm){
+            rmaxm = height[i]
+        }
         rightbound[i] = rmaxm
     }
 
