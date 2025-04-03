e9f8dfbda2d97c898741da3e5950a6842e07c79b
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-04-03 21:30:12 +0530

    Time: 8 ms (100.00%) | Memory: 56.5 MB (66.67%) - LeetSync

--- 1284-four-divisors/four-divisors.js
+++ 1284-four-divisors/four-divisors.js
@@ -3,19 +3,32 @@
  * @return {number}
  */
 var sumFourDivisors = function(nums) {
-    let total = 0
-    for(let i=0; i<nums.length; i++){
-        let count = 0, tempsum = 0
-        for(let div=1; div<= nums[i]; div++){
-            if(count > 4) break
+    let total = 0;
 
-            if(nums[i] % div == 0){
-                count++
-                tempsum += div
+for (let i = 0; i < nums.length; i++) {
+    let count = 0, tempsum = 0;
+    let num = nums[i];
+
+    for (let div = 1; div * div <= num; div++) {
+        if (num % div === 0) {
+            let pair = num / div;
+            
+            // Add both divisors unless they're the same (perfect square case)
+            if (div !== pair) {
+                count += 2;
+                tempsum += div + pair;
+            } else {
+                count++;
+                tempsum += div;
             }
+
+            if (count > 4) break; // Stop early if more than 4 divisors are found
         }
-        if(count == 4) total+= tempsum
     }
-    return total
+
+    if (count === 4) total += tempsum;
+}
+
+return total;
     
 };
