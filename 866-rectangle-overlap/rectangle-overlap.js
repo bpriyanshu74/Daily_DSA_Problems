62df284e4dffce13d5d42d1c519ef87c062a67d3
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-04-05 20:02:16 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (32.10%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.js
+++ 866-rectangle-overlap/rectangle-overlap.js
@@ -4,6 +4,10 @@
  * @return {boolean}
  */
 var isRectangleOverlap = function(rec1, rec2) {
-    
-    return rec1[0] < rec2[2] && rec1[1] < rec2[3] && rec1[2] > rec2[0] && rec1[3] > rec2[1];
+    let [ax1,ay1,ax2,ay2] = rec1
+    let [bx1, by1, bx2, by2] = rec2
+    // over lap does not happen if 1 rec is lft of the other
+    // or above the other
+    // case: left || above
+    return !(ax2 <= bx1 || bx2 <= ax1 || ay2 <= by1 || by2 <= ay1);
 };
