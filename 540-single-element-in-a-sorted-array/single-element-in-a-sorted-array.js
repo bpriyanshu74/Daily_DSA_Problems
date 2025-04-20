8daf8e15a68df4264f093c22ed16ffaa6c9fb364
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-04-20 20:53:37 +0530

    Time: 0 ms (100.00%) | Memory: 55.9 MB (96.18%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
@@ -3,9 +3,21 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-    let l=0, r=nums.length-1
-    while(l<=r){
+    let n = nums.length
+    // i=0
+    if(nums.length == 1 || nums[0] != nums[1]) return nums[0]
+    // i=n-1
+    if(nums[n-1] != nums[n-2]) return nums[n-1]
+
+    // even for first occur
+    // odd for repeat
+
+    let l=1, r= nums.length-2
+    while(l <= r){
         let mid = l + Math.floor((r-l)/2)
+        if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+            return nums[mid]
+        }
         if(mid % 2 == 0){
             if(nums[mid] == nums[mid+1]){
                 l = mid + 1
@@ -14,7 +26,7 @@ var singleNonDuplicate = function(nums) {
                 r = mid - 1
             }
         }
-        else{
+        if(mid % 2 == 1){
             if(nums[mid] == nums[mid-1]){
                 l = mid + 1
             }
@@ -23,5 +35,5 @@ var singleNonDuplicate = function(nums) {
             }
         }
     }
-    return nums[l]
+    return -1
 };
