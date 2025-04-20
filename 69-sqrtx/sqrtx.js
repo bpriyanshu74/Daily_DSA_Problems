a1ed95801bdec733223ffa188ff95bfe088036f5
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-04-20 21:32:44 +0530

    Time: 35 ms (6.73%) | Memory: 55.8 MB (69.83%) - LeetSync

---
+++ 69-sqrtx/sqrtx.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let ans = 0
+    for(let i = 1; i*i <= x; i++){
+        if(i*i <= x){
+            ans = i
+        }
+        else{
+            break
+        }
+    }
+    return ans
+    
+};
