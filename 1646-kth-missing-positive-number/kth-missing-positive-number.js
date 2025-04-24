12792be7f76f0432cf88be176d66e4f36dd5cc43
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-04-24 19:13:14 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (59.96%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.js
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.js
@@ -4,15 +4,14 @@
  * @return {number}
  */
 var findKthPositive = function(arr, k) {
-    let missing = new Array(2100).fill(0)
-
-    for(let i=0; i<arr.length; i++){
-        missing[arr[i]] = 1
+    for(let i=0; i< arr.length; i++){
+        if(arr[i] <= k){
+            k++
+        }
+        else{
+            break
+        }
     }
-    let count = 0
-    for(let i=1; i<missing.length; i++){
-        if(missing[i] == 0) count++
-        if(count == k) return i
-    }
+    return k
     
 };
