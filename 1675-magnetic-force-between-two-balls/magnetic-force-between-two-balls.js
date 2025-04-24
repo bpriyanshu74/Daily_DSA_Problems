036d7d3e53ed55ac60373bfde1181b6c73325d21
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-04-25 02:56:54 +0530

    Time: 135 ms (11.36%) | Memory: 66.5 MB (70.45%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} position
+ * @param {number} m
+ * @return {number}
+ */
+var maxDistance = function(position, m) {
+
+    function can_we_place(pos, dis){
+        let ball_count = 1, last = pos[0]
+        for(let i=1; i<pos.length; i++){
+            if(pos[i] - last >= dis){
+                ball_count++
+                last = pos[i]
+            }
+        }
+        return ball_count
+    }
+    let max = Math.max(...position), min = Math.min(...position), ans = 0
+    position.sort((a,b) => a-b)
+
+    let l = 1, r = max-min
+    while(l<=r){
+        let mid = l + Math.floor((r-l)/2)
+        if(can_we_place(position, mid) >= m){
+            ans = mid
+            l = mid + 1
+        }
+        else{
+            r = mid - 1
+        }
+    }
+    return ans
+};
