7d44cf71a685acc28e6993be0ee891119ccbab06
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-04-30 21:49:54 +0530

    Time: 0 ms (100.00%) | Memory: 60 MB (39.40%) - LeetSync

--- 46-permutations/permutations.js
+++ 46-permutations/permutations.js
@@ -3,26 +3,30 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    function helper(ds,ans,freq,arr){
+    let ans = [], ds = [], visited = new Array(nums.length).fill(false)
+
+    function helper(arr, ans, ds, visited){
+    
         if(ds.length == arr.length){
             ans.push([...ds])
             return
         }
-        for(let i=0; i< nums.length; i++){
-            if(!freq[i]){
-                freq[i] = 1
-                ds.push(nums[i])
-                helper(ds,ans,freq,arr)
+    
+
+        for(let i=0; i<arr.length; i++){
+            if(!visited[i]){
+                visited[i] = true
+                ds.push(arr[i])
+                helper(arr, ans, ds, visited)
+                visited[i] = false
                 ds.pop()
-                freq[i] = 0
+                
             }
+            
+            
         }
-
-        return ans
-
     }
-
-    let ds = [], ans = [], freq = new Array(nums.length).fill(0)
-    return helper(ds,ans,freq,nums)
+    helper(nums,ans,ds,visited)
+    return ans
     
 };
