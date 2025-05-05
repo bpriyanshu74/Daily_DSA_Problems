c676670f592c49dc06f2d33ee0f2095ded439df4
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-05 20:39:29 +0530

    Time: 2 ms (85.21%) | Memory: 59.6 MB (67.94%) - LeetSync

--- 39-combination-sum/combination-sum.js
+++ 39-combination-sum/combination-sum.js
@@ -4,23 +4,26 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-    let ans = [], ds = [], index=0
-    function helper(arr,ans,ds,target,index){
-        if(target == 0){
-            ans.push([...ds])
+    let result = [], cur_candidates = [], k = target, index=0
+    // candidates.sort((a,b) => a-b)
+
+    function helper(arr,k, index){
+        if(k==0){
+            result.push([...cur_candidates])
             return
         }
 
         for(let i=index; i<arr.length; i++){
-            if(arr[i] <= target){
-                ds.push(arr[i])
-                helper(arr,ans,ds,target-arr[i],i)
-                ds.pop()
-            }  
-            
+            // if(i > index && arr[i] == arr[i-1]) continue
+            if(arr[i] <= k){
+                cur_candidates.push(arr[i])
+                helper(arr,k-arr[i],i)
+                cur_candidates.pop()
+            }
+           
         }
     }
-    helper(candidates,ans,ds,target,index)
-    return ans
+    helper(candidates, k, index)
+    return result
     
 };
