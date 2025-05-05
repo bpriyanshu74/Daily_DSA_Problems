48e48fca1aacd9eb62ccd51cb89e14bfa101500c
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-06 00:55:35 +0530

    Time: 9 ms (77.93%) | Memory: 58.6 MB (67.13%) - LeetSync

--- 51-n-queens/n-queens.js
+++ 51-n-queens/n-queens.js
@@ -4,58 +4,52 @@
  */
 var solveNQueens = function(n) {
 
-    let board = new Array(n).fill(null).map(() => new Array(n).fill('.'))
+    let board = new Array(n).fill(null).map(() => new Array(n).fill("."))
     let result = []
-    let row = board.length
-    let col = board[0].length
 
-    function nq(board, row, result){
-        if(row == board.length){
-            // adding the board in result
-            addtoresult(board,result)
+    function nqueens(board,row){
+        if(row == n){
+            saveBoard(board)
             return
         }
-        for(let col = 0; col <board.length; col++){
-            if(isSafe(board,row,col)){
-            board[row][col] = "Q"
-            nq(board, row+1, result)
-            board[row][col] = "."
-            }
-        }
-    }
-    function addtoresult(board, result){
-        let rows = board.length, cols = board.length
-        let temp_result = []
-        for(let r = 0; r<rows; r++){
-            let rowstring = ""
-            for(let c = 0; c<cols; c++){
-            rowstring += board[r][c]
-            }
-            temp_result.push(rowstring)
-        }
-        result.push([...temp_result])
-    }
 
+        for(let col=0; col<n; col++){
+            if(isSafe(board, row, col)){
+                board[row][col] = "Q"
+                nqueens(board,row+1)
+                board[row][col] = "."
+            }
+        }
+    }
     function isSafe(board,row,col){
-        // vertically
-        for(let r = row; r>=0; r--){
+        // check vertically
+        for(let r=row; r>=0; r--){
             if(board[r][col] == "Q") return false
         }
-        // diagonally
-        for(let r = row, c = col; r>=0 && c >= 0; r--,c--){
+        // check positive diagonal
+        for(let r=row,c=col; r >=0 && c >=0 ; r--,c--){
             if(board[r][c] == "Q") return false
         }
-        // -ve diagonal
-        for(let r = row, c = col; r>=0 && c < board.length; r--,c++){
+        // check negative diagonal
+        for(let r=row,c=col; r >=0 && c < n ; r--,c++){
             if(board[r][c] == "Q") return false
         }
         return true
     }
 
-    nq(board,0, result)
+    function saveBoard(board){
+        let cur_combination = []
+        for(let i=0; i<n; i++){
+            let tempstring = ""
+            for(let j=0; j< n; j++){
+                tempstring += board[i][j]
+            }
+            cur_combination.push(tempstring)
+        }
+        result.push([...cur_combination])
+    }
+
+    nqueens(board,0)
     return result
-
-
-
     
 };
