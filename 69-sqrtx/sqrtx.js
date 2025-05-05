deb750b542d26624edff9de0bf62d4ee065f7834
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-06 01:12:51 +0530

    Time: 1 ms (72.57%) | Memory: 55.2 MB (90.64%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -3,15 +3,19 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    let sqrt = 0
-    for(let i=1; i<= x; i++){
-        if(i*i <= x){
-            sqrt = i
+    //  we are doing it using binary search
+    let l = 0, r = x, sqrt = 0
+
+    while(l <= r){
+        let mid = l + Math.floor((r-l)/2)
+        if(mid*mid <= x){
+            sqrt = mid
+            l = mid + 1
         }
         else{
-            break
+            r = mid - 1
         }
     }
-    
     return sqrt
+    
 };
