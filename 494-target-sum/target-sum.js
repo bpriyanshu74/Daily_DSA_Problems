10aaaed89dab7d697dfd38e54311580367e0ca8f
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-07 17:23:35 +0530

    Time: 1689 ms (22.38%) | Memory: 53.7 MB (90.10%) - LeetSync

--- 494-target-sum/target-sum.js
+++ 494-target-sum/target-sum.js
@@ -4,20 +4,19 @@
  * @return {number}
  */
 var findTargetSumWays = function(nums, target) {
-
-    let count = 0, index = 0, cur_exp = "", result = [], sum=0
-
+    let count = 0
     function helper(sum, index){
+
         if(index == nums.length){
             if(sum == target){
                 count++
             }
-            return 
+            return
         }
         helper(sum+nums[index], index+1)
         helper(sum-nums[index], index+1)
     }
-    helper(sum, 0)
+    helper(0,0)
     return count
     
 };
