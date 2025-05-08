3d962c7ea3bf7651ee4a6329dc8721f4d1e8b698
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-08 17:07:04 +0530

    Time: 48 ms (47.75%) | Memory: 54.6 MB (68.09%) - LeetSync

--- 190-reverse-bits/reverse-bits.js
+++ 190-reverse-bits/reverse-bits.js
@@ -4,13 +4,11 @@
  */
 var reverseBits = function(n) {
     let ans = 0
-    for(let cur = 0; cur < 32; cur++){
-        let new_pos = 32 - cur - 1          // reverse position
-        let bit = (n >> cur) & 1            // extract current bit
-        let mask = bit << new_pos           // move to reversed position
-        ans = ans | mask                    // set bit in result
+    for(let i=0; i< 32; i++){
+        let new_pos = 32-1-i
+        let bit = (n >> i) & 1
+        let mask = bit << new_pos
+        ans = ans | mask
     }
     return ans >>> 0
-
-    
 };
