2f658ab3a968ad46c005ece80a3e83ab7d407e77
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-17 15:28:08 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (98.05%) - LeetSync

--- 2561-number-of-distinct-averages/number-of-distinct-averages.js
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.js
@@ -4,15 +4,14 @@
  */
 var distinctAverages = function(nums) {
     nums.sort((a,b) => a-b)
-    // nlogn
-    // 0,1,3,4,4,5
+    let small,large,average
 
     let l=0; r = nums.length-1, avg = new Set()
     while(l<r){
-        let small = nums[l]
-        let large = nums[r]
+        small = nums[l]
+        large = nums[r]
 
-        let average = (small+large)/2
+        average = (small+large)/2
         avg.add(average)
 
         l++
