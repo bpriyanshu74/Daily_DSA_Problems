b36e58c0e67d22fe6fb8bd2a4b314f21f9722cac
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-19 22:27:27 +0530

    Time: 4 ms (76.79%) | Memory: 56.1 MB (26.52%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.js
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.js
@@ -6,7 +6,7 @@ var areOccurrencesEqual = function(s) {
     let hashmap = new Map()
 
     for(let i=0; i<s.length; i++){
-        hashmap.set(s[i], hashmap.get(s[i])+1 || 1)
+        hashmap.set(s[i], (hashmap.get(s[i]) || 0) + 1)
     }
     let basic_count = hashmap.get(s[0])
 
