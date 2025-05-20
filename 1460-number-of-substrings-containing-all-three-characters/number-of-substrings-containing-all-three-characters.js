bf01b912af671acefdfb0ca9afb4843b751df1e4
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-20 22:13:55 +0530

    Time: 9 ms (95.73%) | Memory: 55.1 MB (91.20%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.js
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.js
@@ -5,17 +5,14 @@
 var numberOfSubstrings = function(s) {
     // optimization using two pointer with sliding window
     // hasmap will contain all the three character and the last index they were seen, once all the character are != -1 them we'll pick the min index of the three and add it to the count with + 1. 
-    let count = 0, hashmap = new Map()
-    hashmap.set('a',-1)
-    hashmap.set('b', -1)
-    hashmap.set('c', -1)
-    for(let r = 0; r<s.length; r++){
-        hashmap.set(s[r], r)
+    let count = 0, a=-1, b=-1, c=-1
+    for(let i = 0; i<s.length; i++){
+             if (s[i] === "a") a = i;
+        else if (s[i] === "b") b = i;
+        else c = i;
+
+        if (a !== -1 && b !== -1 && c !== -1) count += Math.min(a, b, c) + 1;
         
-        if(hashmap.get('a') != -1 && hashmap.get('b') != -1 && hashmap.get('c') != -1){
-            let v = [...hashmap.values()]
-            count += Math.min(...v) + 1
-        }
     }
     return count
     
