2706848c37319dbf0a44ad67574fcdc2439e7a3b
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-21 11:50:43 +0530

    Time: 40 ms (42.86%) | Memory: 61.6 MB (93.96%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.js
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.js
@@ -4,21 +4,25 @@
  * @return {number}
  */
 var subarraysWithKDistinct = function(nums, k) {
-    function helper(arr,goal){
-        if(goal < 0) return 0
-        let count = 0, left = 0, right = 0, map = new Map()
-        while(right < arr.length){
-            map.set(arr[right], (map.get(arr[right]) || 0) + 1)
-            while(map.size > goal){
-                map.set(arr[left], map.get(arr[left]) - 1)
-                if(map.get(arr[left]) == 0) map.delete(arr[left])
-                left++
+
+    function atMost(arr,k){
+        let l=0, r=0, total=0, hashset = new Map()
+        while(r < arr.length){
+            hashset.set(arr[r], (hashset.get(arr[r])||0) + 1)
+
+            while(hashset.size > k){
+                hashset.set(arr[l], hashset.get(arr[l])-1)
+                if(hashset.get(arr[l]) == 0){
+                    hashset.delete(arr[l])
+                }
+                l++
             }
-            count += right - left + 1
-            right++
+            total += r-l+1
+            r++
         }
-        return count
+        return total
     }
-    return helper(nums,k) - helper(nums, k-1)
+
+    return atMost(nums,k) - atMost(nums,k-1)
     
 };
