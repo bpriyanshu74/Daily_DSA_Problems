1c032cb7a8d86c6f21329e09b472c9e3e23479a5
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-22 10:47:30 +0530

    Time: 2 ms (70.55%) | Memory: 55.1 MB (47.32%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -6,13 +6,26 @@
 var twoSum = function(nums, target) {
     // brute force:
 
-    for(let i=0; i<nums.length-1; i++){
-        for(let j=i+1; j<nums.length; j++){
-            if(nums[i] + nums[j] == target){
-                return [i,j]
-            }
+    // for(let i=0; i<nums.length-1; i++){
+    //     for(let j=i+1; j<nums.length; j++){
+    //         if(nums[i] + nums[j] == target){
+    //             return [i,j]
+    //         }
+    //     }
+    // }
+    // return [-1,-1]
+    // TC = O(n2)
+
+    let hashmap = new Map()
+
+    for(let i=0; i<nums.length; i++){
+        let diff = target - nums[i]
+
+        if(hashmap.has(diff)){
+            return [i, hashmap.get(diff)]
         }
+
+        hashmap.set(nums[i], i)
     }
     return [-1,-1]
-    
 };
