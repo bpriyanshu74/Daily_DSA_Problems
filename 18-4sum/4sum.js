7e9449ea94aa188c5d350324398df2ae627ce61c
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-22 12:19:02 +0530

    Time: 10 ms (95.00%) | Memory: 57.2 MB (56.87%) - LeetSync

--- 18-4sum/4sum.js
+++ 18-4sum/4sum.js
@@ -4,31 +4,38 @@
  * @return {number[][]}
  */
 var fourSum = function(nums, target) {
-    nums.sort((a,b) => a-b)
-    let res = []
+   
+    // solution is using i,j and then two pointers for rest of the array.
 
-    for(let i=0; i< nums.length; i++){
-        if(i>0 && nums[i] == nums[i-1]) continue
-        for(let j=i+1; j<nums.length; j++){
-            if(j != (i+1) && nums[j] == nums[j-1]) continue
-            let k=j+1, l=nums.length - 1
-            
-            while(k<l){
-                let sum = nums[i] + nums[j] + nums[k] + nums[l]
-                if(sum == target){
-                    let temp = [nums[i], nums[j], nums[k], nums[l]]
-                    res.push(temp)
-                    while(k<l && nums[k] == nums[k+1]) k++
-                    while(k<l && nums[l] == nums[l-1]) l--
-                    k++
-                    l--
+    //1. sort the array
+    nums.sort((a,b) => a-b)
+
+    let quads = [], n = nums.length
+
+    for(let i=0; i<nums.length-3; i++){
+        if(i > 0 && nums[i] == nums[i-1]) continue
+        for(let j=i+1; j<nums.length-2; j++){
+            if(j > i+1 && nums[j] == nums[j-1]) continue
+            let target2 = target - nums[i] - nums[j]
+
+            let l = j+1, r = n-1
+
+            while(l<r){
+                let twosum = nums[l] + nums[r]
+                if(twosum == target2){
+                    let temp = [nums[i], nums[j], nums[l], nums[r]]
+                    quads.push(temp)
+                    while(l < r && nums[l] == temp[2]) l++
+                    while(l < r && nums[r] == temp[3]) r--
                 }
-                else if(sum > target) l--
-                else k++
+                else if(twosum < target2){
+                    l++
+                }
+                else{
+                    r--
+                }
             }
-            
         }
     }
-
-    return res
+    return quads
 };
