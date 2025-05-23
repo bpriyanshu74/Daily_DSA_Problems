ec628a5ccdd1795811bbef4bc6da7bc15536603c
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-23 12:45:38 +0530

    Time: 14 ms (44.51%) | Memory: 61.4 MB (39.63%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.js
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.js
@@ -5,19 +5,19 @@
  */
 var findPairs = function(nums, k) {
     if(k < 0) return 0
-
+    
     let hashmap = new Map(), count = 0
 
     for(let i=0; i<nums.length; i++){
-        hashmap.set(nums[i], (hashmap.get(nums[i]) || 0) + 1)
+        hashmap.set(nums[i], (hashmap.get(nums[i])||0)+1)
     }
 
-    for(let [key,value] of hashmap.entries()){
+    for(let [key,val] of hashmap.entries()){
         if(k == 0){
-            if(value > 1) count++
+            if(val > 1) count++
         }
         else{
-            if(hashmap.has(key+k)) count++
+            if(hashmap.has(key+k))count++
         }
     }
     return count
