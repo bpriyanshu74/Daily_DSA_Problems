a84ddc49f7a662e364f3e4e367602202d095a260
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-26 23:11:12 +0530

    Time: 7 ms (98.51%) | Memory: 56.8 MB (67.58%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.js
+++ 76-minimum-window-substring/minimum-window-substring.js
@@ -4,7 +4,7 @@
  * @return {string}
  */
 var minWindow = function(s, t) {
-    let minlength = 100000, n = s.length, m = t.length
+    let minlength = 1000000, n = s.length, m = t.length, sindex = -1, l=0, count = 0
 
     let hasharray = new Array(256).fill(0)
 
@@ -13,26 +13,26 @@ var minWindow = function(s, t) {
         hasharray[index]++
     }
 
-    let l=0, count = 0, sindex = -1
-
-    for(let r=0; r<n; r++){
+    for(let r=0; r<s.length; r++){
         let index = s[r].charCodeAt(0)
+
         if(hasharray[index] > 0) count++
         hasharray[index]--
 
         while(count == m){
+            let lindex = s[l].charCodeAt(0)
+          
             if(r-l+1 < minlength){
                 minlength = r-l+1
                 sindex = l
             }
-            let lindex = s[l].charCodeAt(0)
+            if(hasharray[lindex] == 0) count--
             hasharray[lindex]++
-
-            if(hasharray[lindex] > 0) count-=1
-
             l++
+            
         }
     }
 
-    return sindex == -1 ? "" : s.slice(sindex, sindex + minlength)
+
+    return sindex === -1 ? "" : s.slice(sindex, sindex+minlength)
 };
