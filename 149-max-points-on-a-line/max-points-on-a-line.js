653ddc6ac75e69b0068c028c5e9ef5cfeb9459e1
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-05-26 13:13:52 +0530

    Time: 198 ms (9.65%) | Memory: 60.5 MB (66.23%) - LeetSync

---
+++ 149-max-points-on-a-line/max-points-on-a-line.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var maxPoints = function(points) {
+    if(points.length <= 2) return points.length
+    // brute force
+    let maxpoints = 0
+    for(let i=0; i<points.length; i++){
+        for(let j=0; j<points.length; j++){
+            if(i==j) continue
+
+            let [x1,y1] = points[i], [x2,y2] = points[j]
+
+            let slope = (y2-y1)/(x2-x1), count = 2
+
+            for(let k=0; k<points.length; k++){
+                if(points[k] == points[i] || points[k] == points[j]) continue
+
+                let [x3,y3] = points[k]
+
+                let slope2 = (y3-y1)/(x3-x1)
+                let slope3 = (y3-y2)/(x3-x2)
+
+                if(slope2 == slope3) count++
+            }   
+
+            maxpoints = Math.max(count, maxpoints)
+        }
+    }
+    return maxpoints
+    
+};
