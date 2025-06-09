d007f497f9ffedb2d21714738edcff15a38bb6df
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-06-09 21:27:51 +0530

    Time: 81 ms (26.70%) | Memory: 56.3 MB (97.74%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.js
+++ 149-max-points-on-a-line/max-points-on-a-line.js
@@ -3,49 +3,31 @@
  * @return {number}
  */
 var maxPoints = function(points) {
-    if(points.length <= 2) return points.length
-    // brute force
-    // let maxpoints = 0
-    // for(let i=0; i<points.length; i++){
-    //     for(let j=0; j<points.length; j++){
-    //         if(i==j) continue
+    let maxpoints = 1
 
-    //         let [x1,y1] = points[i], [x2,y2] = points[j]
+    for(let i=0; i< points.length; i++){
+        let x1 = points[i][0], y1 = points[i][1]
+        for(let j=0; j< points.length; j++){
+            if(i == j) continue
+            let x2 = points[j][0], y2 = points[j][1]
 
-    //         let slope = (y2-y1)/(x2-x1), count = 2
+            let slope1 = (y2-y1)/(x2-x1), cur_points = 2
 
-    //         for(let k=0; k<points.length; k++){
-    //             if(points[k] == points[i] || points[k] == points[j]) continue
+            for(let k=0; k<points.length; k++){
+                if(i==k || j==k) continue
 
-    //             let [x3,y3] = points[k]
+                let x3 = points[k][0], y3 = points[k][1]
 
-    //             let slope2 = (y3-y1)/(x3-x1)
-    //             let slope3 = (y3-y2)/(x3-x2)
+                let slope2 = (y3-y1)/(x3-x1)
 
-    //             if(slope2 == slope3) count++
-    //         }   
-
-    //         maxpoints = Math.max(count, maxpoints)
-    //     }
-    // }
-    // return maxpoints
-
-    // now we can optimize this o(n3) to o(n2) using hashmap
-    let maxpoints = 0
-
-    for(let i=0; i<points.length; i++){
-        let hashmap = new Map()
-        for(let j=0; j<points.length; j++){
-            if(i==j) continue
-            let [x1,y1] = points[i], [x2,y2] = points[j]
-            let slope = (y2-y1)/(x2-x1)
-
-            hashmap.set(slope, (hashmap.get(slope)||1)+1)
-        }
-        for(let values of hashmap.values()){
-            maxpoints = Math.max(maxpoints, values)
+                if(slope2 == slope1){
+                    cur_points++
+                }
+            }
+            maxpoints = Math.max(cur_points, maxpoints)
         }
     }
+
     return maxpoints
     
 };
