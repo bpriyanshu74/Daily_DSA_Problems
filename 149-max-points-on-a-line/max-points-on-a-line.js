1a2b866b902daea19b8bb509ca787baa8e6eba48
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-06-09 21:33:32 +0530

    Time: 12 ms (90.95%) | Memory: 60 MB (73.30%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.js
+++ 149-max-points-on-a-line/max-points-on-a-line.js
@@ -5,26 +5,18 @@
 var maxPoints = function(points) {
     let maxpoints = 1
 
-    for(let i=0; i< points.length; i++){
+    for(let i=0; i<points.length; i++){
         let x1 = points[i][0], y1 = points[i][1]
-        for(let j=0; j< points.length; j++){
-            if(i == j) continue
+        let hashmap = new Map()
+        for(let j=0; j<points.length; j++){
+            if(i==j) continue
             let x2 = points[j][0], y2 = points[j][1]
 
-            let slope1 = (y2-y1)/(x2-x1), cur_points = 2
+            let slope = (y2-y1)/(x2-x1)
 
-            for(let k=0; k<points.length; k++){
-                if(i==k || j==k) continue
+            hashmap.set(slope, (hashmap.get(slope)|| 1) + 1)
 
-                let x3 = points[k][0], y3 = points[k][1]
-
-                let slope2 = (y3-y1)/(x3-x1)
-
-                if(slope2 == slope1){
-                    cur_points++
-                }
-            }
-            maxpoints = Math.max(cur_points, maxpoints)
+            maxpoints = Math.max(maxpoints, hashmap.get(slope))
         }
     }
 
