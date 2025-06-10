4b7e8730bf15766eaf3494d23d5d1dfe2c0964c2
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-06-10 12:11:43 +0530

    Time: 3 ms (56.86%) | Memory: 61.3 MB (64.68%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.js
+++ 2-add-two-numbers/add-two-numbers.js
@@ -11,49 +11,24 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    if(l1 == null) return l2
-    if(l2 == null) return l1
-    if(!l1 && !l2) return null
+    let dummy = new ListNode(-1)
+    let cur = dummy, carry = 0
 
-    let dummy = new ListNode(-1),cur = dummy, carry = 0
+    while(l1 || l2 || carry){
+        let val1 = l1 ? l1.val : 0
+        let val2 = l2 ? l2.val : 0
 
-    while(l1 && l2){
-        let sum = l1.val + l2.val + carry
-        let newdigit = (sum)% 10
+        let sum = val1+val2+carry
+
+        let newdigit = new ListNode(sum % 10)
         carry = Math.floor(sum / 10)
 
-        let newnode = new ListNode(newdigit)
+        cur.next = newdigit
+        cur = cur.next
 
-        cur.next = newnode
-        cur = cur.next
-        l1 = l1.next
-        l2 = l2.next
+        if(l1) l1 = l1.next
+        if(l2) l2 = l2.next
     }
-    while(l1){
-        let sum = l1.val + carry
-        let newdigit = (sum)% 10
-        carry = Math.floor(sum / 10)
-        let newnode = new ListNode(newdigit)
-        cur.next = newnode
-        cur = cur.next
-        l1 = l1.next
-    }
-    while(l2){
-        let sum = l2.val + carry
-        let newdigit = (sum)% 10
-        carry = Math.floor(sum / 10)
-        let newnode = new ListNode(newdigit)
-        cur.next = newnode
-        cur = cur.next
-        l2 = l2.next
-    }
-
-    if(carry != 0){
-        let newnode = new ListNode(carry)
-        cur.next = newnode
-        cur = cur.next
-    }
-
 
     return dummy.next
     
