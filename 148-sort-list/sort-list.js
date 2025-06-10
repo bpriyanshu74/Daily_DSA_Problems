a2212f45092863c83933b69008b5d7bdb010d648
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-06-10 15:44:35 +0530

    Time: 12 ms (98.33%) | Memory: 77.7 MB (90.90%) - LeetSync

--- 148-sort-list/sort-list.js
+++ 148-sort-list/sort-list.js
@@ -10,50 +10,26 @@
  * @return {ListNode}
  */
 var sortList = function(head) {
+    let list = []
 
-    // write tortoise hare algo to find the middle
-    function findMiddle(head){
-        let slow = head
-        let fast = head.next
-        while(fast && fast.next){
-            slow = slow.next
-            fast = fast.next.next
-        }
-        return slow
+    let temp = head, temp1 = head
+
+    while(temp){
+        list.push(temp.val)
+        temp = temp.next
     }
 
-    function merge(list1,list2){
-        let dummy = new ListNode(-1000000)
-        let temp = dummy
+    list.sort((a,b) => a-b)
 
-        while(list1 != null && list2 != null){
-            if(list1.val <= list2.val){
-                temp.next = list1
-                temp = list1
-                list1 = list1.next
-            }
-            else{
-                temp.next = list2
-                temp = list2
-                list2 = list2.next
-            }
-        }
-        if(list1) temp.next = list1
-        else temp.next = list2
+    let i=0
 
-        return dummy.next
+    while(temp1){
+        temp1.val = list[i]
+        i++
+        temp1 = temp1.next
     }
 
-    if(head == null || head.next == null) return head
+    return head
 
-    let middle = findMiddle(head)
     
-    let right = middle.next
-    middle.next = null
-    let left = head
-
-    let leftHead = sortList(left)
-    let rightHead = sortList(right)
-
-    return merge(leftHead, rightHead)
 };
