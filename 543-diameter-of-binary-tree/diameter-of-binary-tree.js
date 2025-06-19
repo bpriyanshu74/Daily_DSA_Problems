62e56722763a3e31dca546a7b411f3da199ac54a
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-06-19 12:21:35 +0530

    Time: 1 ms (86.24%) | Memory: 62.5 MB (43.65%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.js
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.js
@@ -13,26 +13,23 @@
 var diameterOfBinaryTree = function(root) {
     if(root == null) return 0
     let maxm = 0
-    diameter(root)
+
+    findh(root)
+
+    return maxm
     
-    function diameter(root){
-        if(root == null) return 0
-
-        let lefth = findh(root.left)
-        let righth = findh(root.right)
-
-        maxm = Math.max(maxm, lefth+righth)
-
-        diameter(root.left)
-        diameter(root.right)
-    }
 
     function findh(root){
         if(root == null) return 0
 
-        return 1 + Math.max(findh(root.left), findh(root.right))
+        let left = findh(root.left)
+        let right = findh(root.right)
+
+        maxm = Math.max(maxm, left+right)
+
+        return 1 + Math.max(left,right)
     }
 
-    return maxm
+    
     
 };
