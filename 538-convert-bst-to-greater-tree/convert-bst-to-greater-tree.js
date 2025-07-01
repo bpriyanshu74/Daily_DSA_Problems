d4b38320a24d7fa813308fdfc00ce68c53251f72
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-01 23:24:22 +0530

    Time: 1 ms (60.71%) | Memory: 64.5 MB (69.64%) - LeetSync

--- 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.js
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.js
@@ -11,35 +11,18 @@
  * @return {TreeNode}
  */
 var convertBST = function(root) {
+    let sum = 0
+    function reverseInorder(node){
+        if(!node) return 0
 
-    function inorderCollect(node){
-        if(!node) return
-        inorderCollect(node.left)
-        inorder.push(node.val)
-        inorderCollect(node.right)
+        reverseInorder(node.right)
+        sum += node.val
+        node.val = sum
+        reverseInorder(node.left)
+
     }
 
-    let inorder = []
-    inorderCollect(root)
-    let acc_sum = 0
-    for(let i=inorder.length-1; i>= 0; i--){
-        acc_sum += inorder[i]
-        inorder[i] = acc_sum
-    }
-
-    let index = {val: 0}
-
-    function dfsUpdate(node, index){
-        if(!node) return
-
-        dfsUpdate(node.left, index)
-        node.val = inorder[index.val]
-        index.val++
-        dfsUpdate(node.right, index)
-    }
-
-    dfsUpdate(root, index)
-
+    reverseInorder(root)
     return root
     
 };
