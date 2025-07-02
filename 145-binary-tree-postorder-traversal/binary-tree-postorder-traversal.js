7ab1bc5a10052b3f4641412bc59972c0eb4d922e
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-02 14:33:22 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (48.75%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
@@ -11,35 +11,16 @@
  * @return {number[]}
  */
 var postorderTraversal = function(root) {
-    if(root == null) return []
+    let post = []
 
-    let stack = [], cur = root, post = []
-
-    while(cur != null || stack.length != 0){
-        if(cur != null){
-            stack.push(cur)
-            cur = cur.left
-        }
-        else{
-            let temp = stack[stack.length-1].right
-            if(temp == null){
-                temp = stack[stack.length-1]
-                stack.pop()
-                post.push(temp.val)
-
-                while(stack.length != 0 && temp == stack[stack.length-1].right){
-                    temp = stack[stack.length-1]
-                    stack.pop()
-                    post.push(temp.val)
-                }
-
-            }
-            else{
-                cur = temp
-            }
-        }
+    function dfs(node){
+        if(!node) return
+        dfs(node.left)
+        dfs(node.right)
+        post.push(node.val)
     }
 
+    dfs(root)
     return post
     
 };
