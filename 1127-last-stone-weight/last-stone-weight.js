ccb2b1be3a0c940b1fb430ea3b942f4394efedea
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-09 13:27:46 +0530

    Time: 3 ms (51.67%) | Memory: 56.6 MB (16.53%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.js
+++ 1127-last-stone-weight/last-stone-weight.js
@@ -3,22 +3,72 @@
  * @return {number}
  */
 var lastStoneWeight = function(stones) {
-    if(stones.length == 0) return 0
-    // min_heap
+    let heap = [], size = 0
 
-    let minheap = stones.sort((a,b) => a-b)
-    let heapsize = minheap.length
+    for(let i=0; i<stones.length; i++){
+        insert(stones[i])
+    }
+    function insert(val){
+        heap.push(val)
+        size += 1
+        heapifyUp()
+    }
+    function extract(){
+        [heap[0], heap[size-1]] = [heap[size-1], heap[0]]
+        size -= 1
+        let maxval = heap.pop()
+        heapifyDown()
 
-    while(heapsize != 1){
-        let one = heapsize-1
-        let two = heapsize-2
-
-        let newstone = minheap[one] - minheap[two]
-        minheap[two] = newstone
-        minheap.sort((a,b) => a-b)
-        heapsize--
+        return maxval
     }
 
-    return minheap[0]
+    function heapifyUp(){
+        let index = size-1
+        while(index > 0){
+            let parentindex = Math.floor((index-1)/2)
+
+            if(heap[index] > heap[parentindex]){
+                [heap[index], heap[parentindex]] = [heap[parentindex], heap[index]]
+                index = parentindex
+            }
+            else{
+                break
+            }
+        }
+    }
+
+    function heapifyDown(){
+        let index = 0
+        while(true){
+            let largest = index, left = 2*index+1, right = 2*index+2
+            if(left < size && heap[left] > heap[largest]){
+                largest = left
+            }
+            if(right < size && heap[right] > heap[largest]){
+                largest = right
+            }
+            if(largest != index){
+                [heap[largest], heap[index]] = [heap[index], heap[largest]]
+                index = largest
+            }
+            else{
+                break
+            }
+        }
+    }
+
+    while(size > 1){
+        let first = extract()
+        let second = extract()
+
+        let newstone = first - second
+
+        if(newstone > 0){
+            insert(newstone)
+        }
+    }
+
+    if(size > 0) return heap[0]
+    return 0
     
 };
