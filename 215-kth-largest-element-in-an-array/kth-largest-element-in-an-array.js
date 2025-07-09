0c65580ce9ed4af29597f184352ca7fd38851a4a
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-09 13:30:51 +0530

    Time: 81 ms (34.55%) | Memory: 65.2 MB (75.01%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    nums.sort((a,b) => a-b)
+    return nums[nums.length - k]
+    
+};
