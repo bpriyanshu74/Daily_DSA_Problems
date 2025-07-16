55e114b2b784a1bb20345de6b2c90b01315fe86b
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-16 16:34:49 +0530

    Time: 26 ms (68.57%) | Memory: 63 MB (17.23%) - LeetSync

--- 455-assign-cookies/assign-cookies.js
+++ 455-assign-cookies/assign-cookies.js
@@ -4,23 +4,18 @@
  * @return {number}
  */
 var findContentChildren = function(g, s) {
+    let i = 0
     g.sort((a,b) => a-b)
     s.sort((a,b) => a-b)
 
-    // left tracks the children, right tracks the cookies
-
-    let r = 0, l = 0
-    while(r < s.length){
-        if(s[r] >= g[l]){
-            l++
-            r++
-        }
-        else{
-            r++
+    for(let k = 0; k<s.length; k++){
+        if(s[k] >= g[i]){
+            i++
         }
     }
 
-    return l
+    return i
 
-
+    
+    
 };
