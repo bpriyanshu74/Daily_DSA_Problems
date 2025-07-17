5bc1751d8b3f92dc35d8d211c076057cfc4374fc
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-17 16:18:32 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (67.50%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -3,29 +3,29 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    if(digits == "") return []
-    let mapping = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+    if(digits.length == 0) return []
 
-    function helper(digits, ans, cur, index){
+    let ans = [], ds = [], index = 0
+    let map = ['','', 'abc', 'def','ghi','jkl','mno','pqrs','tuv','wxyz']
+
+    function dfs(ans,ds, index){
         if(index == digits.length){
-            ans.push(cur.join(""))
+            ans.push(ds.join(''))
             return
         }
 
         let number = digits[index] - "0"
-        let values = mapping[number]
+        let values = map[number]
 
-        for(let char = 0; char < values.length; char++){
-            cur.push(values[char])
-            helper(digits,ans, cur, index+1)
-            cur.pop()
+        for(let i=0; i<values.length; i++){
+            ds.push(values[i])
+            dfs(ans, ds, index+1)
+            ds.pop()
         }
     }
+    dfs(ans, ds, index)
 
-    let ans = [], cur = [], index=0
-    helper(digits,ans,cur,index)
     return ans
-
     
 
     
