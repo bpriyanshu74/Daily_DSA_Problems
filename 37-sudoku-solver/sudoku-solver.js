5506f2601c8ab86b89ac60d5060377b58b0af6ab
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-21 23:41:03 +0530

    Time: 521 ms (48.17%) | Memory: 56.7 MB (56.86%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.js
+++ 37-sudoku-solver/sudoku-solver.js
@@ -3,42 +3,40 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solveSudoku = function(board) {
-    helper(board)
 
-    function helper(board){
+    function isSafe(board, row, col, number){
+        // checking row
+        for(let i=0; i<9; i++){
+            if(board[row][i] == number) return false
+            if(board[i][col] == number) return false
+            let boxRow = 3 * Math.floor(row/3) + Math.floor(i/3)
+            let boxCol = 3 * Math.floor(col/3) + (i%3)
+            if(board[boxRow][boxCol] == number) return false
+
+        }
+        return true
+        
+    }
+
+    function dfs(){
         for(let i=0; i<9; i++){
             for(let j=0; j<9; j++){
-                if(board[i][j] == "."){
-                    for(let c = 1; c<=9; c++){
-                        if(isValid(board,i,j,String(c))){
-                            board[i][j] = String(c)
-
-                            if(helper(board) == true){
-                                return true
-                            }
-                            else{
-                                board[i][j] = "."
-                            }
+                if(board[i][j] == '.'){
+                    for(let k = 1; k<= 9; k++){
+                        let ch = k.toString()
+                        if(isSafe(board,i,j, ch)){
+                            board[i][j] = ch
+                            if(dfs()) return true
+                            board[i][j] = '.'
                         }
                     }
                     return false
                 }
-                
             }
         }
         return true
     }
-
-    function isValid(board,row,col,num){
-        for(let i=0; i<9; i++){
-            if(board[row][i] == num) return false
-
-            if(board[i][col] == num) return false
-
-            if(board[3 * Math.floor(row/3) + Math.floor(i/3)][3 * Math.floor(col/3) + i%3] == num) return false
-        }
-        return true
-    }
-    
+    dfs()
+ 
     
 };
