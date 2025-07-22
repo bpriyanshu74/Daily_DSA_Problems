407c92e11d4aa8f3bd53d7cfb2c5e7286d8aeede
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-22 17:22:33 +0530

    Time: 2 ms (77.69%) | Memory: 58.7 MB (79.90%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -3,25 +3,23 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    let rows_with_zero = new Set()
-    let cols_with_zero = new Set()
-
-    for(let i=0; i<matrix.length; i++){
-        for(let j=0; j<matrix[0].length; j++){
+    let zero_rows = new Set(), zero_cols = new Set()
+    for(let i=0; i< matrix.length; i++){
+        for(let j=0; j< matrix[0].length; j++){
             if(matrix[i][j] == 0){
-                rows_with_zero.add(i)
-                cols_with_zero.add(j)
+                zero_rows.add(i)
+                zero_cols.add(j)
             }
         }
     }
 
     for(let i=0; i<matrix.length; i++){
-        for(let j=0; j<matrix[0].length; j++){
-            if(rows_with_zero.has(i) || cols_with_zero.has(j)){
+        for(let j=0; j< matrix[0].length; j++){
+            if(zero_rows.has(i) || zero_cols.has(j)){
                 matrix[i][j] = 0
             }
         }
     }
-    
+    return matrix
     
 };
