6471b1fef317304f0ee77a011cba9e47c3b4ac65
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-25 11:15:51 +0530

    Time: 7 ms (85.67%) | Memory: 64.1 MB (81.15%) - LeetSync

--- 56-merge-intervals/merge-intervals.js
+++ 56-merge-intervals/merge-intervals.js
@@ -7,16 +7,15 @@ var merge = function(intervals) {
     intervals.sort((a,b) => a[0]-b[0])
     let res = []
 
-    let temp = intervals[0]
+    let temp = [...intervals[0]]
 
     for(let i=1; i<intervals.length; i++){
-        if(intervals[i][0] > temp[1]){
-            res.push(temp)
-            temp = intervals[i]
+        if(intervals[i][0] <= temp[1]){
+            temp[1] = Math.max(temp[1], intervals[i][1])
         }
         else{
-            temp[0] = Math.min(temp[0], intervals[i][0])
-            temp[1] = Math.max(temp[1], intervals[i][1])
+            res.push(temp)
+            temp = intervals[i]
         }
     }
     res.push(temp)
