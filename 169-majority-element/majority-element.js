c8cdd02714d70448041088ed0c81aa5d67d71bf2
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-26 00:20:09 +0530

    Time: 2 ms (72.96%) | Memory: 57.5 MB (46.86%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,18 +3,13 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let majority = Math.floor(nums.length / 2)
-    let n = nums.length
-    let frequency = new Map()
+    let n = Math.floor(nums.length/2)
+
+    let hashmap = new Map()
 
     for(let num of nums){
-        frequency.set(num, (frequency.get(num) || 0) + 1);
-    }
-
-    for(let num of nums){
-        if(frequency.get(num) > majority){
-            return num
-        }
+        hashmap.set(num, (hashmap.get(num)||0)+1)
+        if(hashmap.get(num) > n) return num
     }
     
 };
