34635322c5038d124f97349bebfb7585fe87e9cc
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-26 00:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (69.26%) - LeetSync

--- 50-powx-n/powx-n.js
+++ 50-powx-n/powx-n.js
@@ -4,18 +4,20 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-    if(n == 0) return 1
-
     if(n < 0){
         x = 1/x
         n = -n
     }
 
-    if(n%2 == 0){
-        return myPow(x*x, n/2)
+    function power(x,n){
+        if(n==0) return 1
+        if(n % 2 == 0){
+            return power(x*x, n/2)
+        }
+        else{
+            return x*power(x,n-1)
+        }
     }
-    if(n%2 != 0){
-        return x*myPow(x, n-1)
-    }
+    return power(x,n)
     
 };
