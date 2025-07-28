cf8c9234dd277d88a21ae865dadd7b32c6323e79
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-28 12:37:00 +0530

    Time: 39 ms (39.94%) | Memory: 71 MB (93.49%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,39 +3,31 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let n = nums.length
     let res = []
     nums.sort((a,b) => a-b)
-    for(let i=0; i<n-2; i++){
-        if(i>0 && nums[i] == nums[i-1]) continue;
 
-        let cur = nums[i]
-        let target = -cur
+    for(let i=0; i<nums.length-2; i++){
+        if(i > 0 && nums[i] == nums[i-1]) continue
 
-        let low=i+1,high=n-1
+        let l = i+1, r = nums.length-1
 
-        
-
-        while(low<high){
-            let cursum = nums[low] + nums[high]
-            if(cursum == target){
-                res.push([nums[i], nums[low], nums[high]])
-
-                while(low<high && nums[low] == nums[low+1]) low++;
-
-                while(low<high && nums[high] == nums[high-1]) high--;
-
-                low++;
-                high--;
+        while(l < r){
+            let sum = nums[i] + nums[l] + nums[r]
+            if(sum == 0){
+                res.push([nums[i], nums[l], nums[r]])
+                while(l < r && nums[l] == nums[l+1]) l++
+                while(l < r && nums[r] == nums[r-1]) r-- 
+                l++
+                r--
             }
-            else if(cursum > target){
-                high--;
+            else if(sum < 0){
+                l++
             }
             else{
-                low++;
+                r--
             }
-            
         }
     }
-    return Array.from(res)
+    return res
+    
 };
