834c656801210f5803b762e6a70a4cf165d55345
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-29 23:20:51 +0530

    Time: 0 ms (100.00%) | Memory: 56.6 MB (81.19%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -11,62 +11,35 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    if(list1 == null) return list2
-    if(list2 == null) return list1
+    if(!list1) return list2
+    if(!list2) return list1
+    let dummy = new ListNode(-1), temp = dummy
+    let temp1 = list1, temp2 = list2
 
-    // let dummy = new ListNode(-1)
-    // let cur = dummy
-
-    // while(list1 && list2){
-    //     if(list1.val <= list2.val){
-    //         cur.next = list1
-    //         list1 = list1.next
-    //     }
-    //     else{
-    //         cur.next = list2
-    //         list2 = list2.next
-    //     }
-    //     cur = cur.next
-    // }
-
-    // if(list1) cur.next = list1
-    // if(list2) cur.next = list2
-
-    // return dummy.next
-
-    // lets try inplace
-    let res
-    
-    if(list1.val <= list2.val){
-        cur = list1
-        list1 = list1.next
-        res = cur
-    }
-    else{
-        cur = list2
-        list2 = list2.next
-        res = cur
-    }
-
-
-
-    while(list1 && list2){
-        if(list1.val <= list2.val){
-            cur.next = list1
-            list1 = list1.next
+    while(temp1 && temp2){
+        if(temp1.val <= temp2.val){
+            temp.next = temp1
+            temp1 = temp1.next
         }
         else{
-            cur.next = list2
-            list2 = list2.next
+            temp.next = temp2
+            temp2 = temp2.next
         }
-        cur = cur.next
+        temp = temp.next
     }
 
-    if(list1) cur.next = list1
-    if(list2) cur.next = list2
+    while(temp1){
+        temp.next = temp1
+        temp1 = temp1.next
+        temp = temp.next
+    }
 
-    return res
+    while(temp2){
+        temp.next = temp2
+        temp2 = temp2.next
+        temp = temp.next
+    }
 
-
+    return dummy.next
     
 };
