45aaa095da1348cfc514e59aa75dc87ad1765657
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-07-31 21:51:13 +0530

    Time: 11 ms (35.61%) | Memory: 82.4 MB (36.20%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -10,27 +10,17 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-
-    let slow = head, fast = head, temp = head
-
-    while(fast && fast.next){
-        slow = slow.next
-        fast = fast.next.next
-    }
-
-    // reversing the second half
-    let cur = slow, prev = null
-    while(cur){
-        let nxt = cur.next
-        cur.next = prev
-        prev = cur
-        cur = nxt
-    }
-
-    while(temp && prev){
-        if(temp.val != prev.val) return false
+    let arr = [], temp = head
+    while(temp){
+        arr.push(temp.val)
         temp = temp.next
-        prev = prev.next
+    }
+
+    let l=0, r = arr.length-1
+    while(l < r){
+        if(arr[l] != arr[r]) return false
+        l++
+        r--
     }
     return true
     
