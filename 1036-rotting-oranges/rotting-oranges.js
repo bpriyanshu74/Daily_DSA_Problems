9f1fe775b644609c644d71397b91b681b37daa61
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-08-07 11:04:55 +0530

    Time: 18 ms (13.08%) | Memory: 61.4 MB (12.25%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.js
+++ 1036-rotting-oranges/rotting-oranges.js
@@ -3,42 +3,35 @@
  * @return {number}
  */
 var orangesRotting = function(grid) {
-    let n = grid.length, m = grid[0].length, time = 0, q = []
-    let visited = new Array(n).fill().map(() => new Array(m).fill(0))
-    let directions = [[1,0], [0,1], [-1,0], [0,-1]]
-    let freshcount = 0
-    // pushing all the rotten oranges in the queue
-    for(let i=0; i<n ; i++){
-        for(let j=0; j<m; j++){
+    let m = grid.length, n = grid[0].length, q = []
+    let maxtime = 0, fresh = 0, directions = [[1,0], [0,1], [-1,0], [0,-1]]
+    // puttin all the rotten oranges in the queue
+    for(let i=0; i<m; i++){
+        for(let j=0; j<n; j++){
             if(grid[i][j] == 2){
-                q.push([i,j])
+                q.push([[i,j], maxtime])
             }
             if(grid[i][j] == 1){
-                freshcount++
+                fresh++
             }
         }
     }
 
-    while(q.length && freshcount > 0){
-        let size = q.length
-        for(let i=0; i< size; i++){
-            let [i,j] = q.shift()
-            visited[i][j] = 1
-            for(let [di,dj] of directions){
-                let dx = i+di, dy = j+dj
-                if(dx < n && dx >= 0 && dy < m && dy >= 0){
-                    if(grid[dx][dy] == 1 && !visited[dx][dy]){
-                        freshcount--
-                        visited[dx][dy] = 1
-                        q.push([dx,dy])
-                        grid[dx][dy] = 2
-                    }
-                }
+    while(q.length){
+        let [cur, time] = q.shift()
+        let [i,j] = cur
+        maxtime = Math.max(maxtime,time)
+        for(let [di, dj] of directions){
+            let dx = i + di, dy = j + dj
+            if(dx<m && dx >= 0 && dy <n && dy >= 0 && grid[dx][dy] == 1){
+                q.push([[dx,dy], time+1])
+                grid[dx][dy] = 2
+                fresh--
             }
         }
-        time++
     }
 
-    return freshcount ? -1 : time
+    return fresh ? -1 : maxtime
+
     
 };
