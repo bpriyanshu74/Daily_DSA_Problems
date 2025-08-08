57201461d0d847877cbad132ce6327b26887ddba
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-08-08 13:34:17 +0530

    Time: 1 ms (87.94%) | Memory: 58.2 MB (15.87%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.js
+++ 801-is-graph-bipartite/is-graph-bipartite.js
@@ -3,27 +3,24 @@
  * @return {boolean}
  */
 var isBipartite = function(graph) {
-    // lets do this using bfs
     let color = new Array(graph.length).fill(-1)
     for(let i=0; i<graph.length; i++){
         if(color[i] == -1){
             color[i] = 0
-            let q = [i]
-        
-        while(q.length){
-            let node = q.shift()
-            for(let nbr of graph[node]){
-                if(color[nbr] == -1){
-                    color[nbr] = 1 - color[node]
-                    q.push(nbr)
-                }else if(color[nbr] == color[node]){
-                        return false  
-                }
-            }
-        }
+            if(!dfs(color, graph, i)) return false
         }
     }
-    
     return true
+
+    function dfs(color, graph, node){
+        for(let nbr of graph[node]){
+            if(color[nbr] == -1){
+                color[nbr] = 1 - color[node]
+                if(!dfs(color, graph, nbr)) return false
+            }
+            else if(color[nbr] == color[node]) return false
+        }
+        return true
+    }
     
 };
