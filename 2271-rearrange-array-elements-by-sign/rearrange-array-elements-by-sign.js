2b8fbbc5e531a563e1ce8703d040e336f43df811
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-04 18:02:25 +0530

    Time: 13 ms (79.60%) | Memory: 98.8 MB (36.96%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function(nums) {
+    // even index for even numbers
+    // odd index for negative numbers
+    let n = nums.length
+    let temp = new Array(n).fill(0),x=0,y=1
+
+    for(let i=0; i<n; i++){
+        if(nums[i] >= 0){
+            temp[x] = nums[i]
+            if(x+2 <= n-1){
+                x+=2
+            }
+        }else{
+            temp[y] = nums[i]
+            if(y+2 <= n-1){
+                y+=2
+            }
+        }
+    }
+
+    return temp
+    
+};
