8b26b4da2b0414bb68cab866c3c3dc0cf4e4f4ed
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-04 17:22:03 +0530

    Time: 2 ms (72.98%) | Memory: 63.6 MB (59.18%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -3,16 +3,20 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let l = 0, r = 0, maxsum = -Infinity, sum = 0
-    while(r < nums.length){
-        sum += nums[r]
-        r++
-        maxsum = Math.max(sum, maxsum)
-        if(sum < 0){
-            sum = 0
+    // brute force
+    let maxsum = nums[0]
+    let i=1, cursum = nums[0]
+
+    while(i < nums.length){
+        if(cursum < 0){
+            cursum = nums[i]
         }
+        else{
+            cursum += nums[i]
+        }
+        maxsum = Math.max(cursum, maxsum)
+        i++
     }
 
     return maxsum
-    
 };
