b2e843fe909db63db6a90d2605abd531daabeedf
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-04 08:32:50 +0530

    Time: 0 ms (100.00%) | Memory: 56.1 MB (57.86%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -4,9 +4,10 @@
  */
 var singleNumber = function(nums) {
     let ans = 0
-    for(let i=0; i<nums.length;i++){
-        ans = ans ^ nums[i]
+    for(let num of nums){
+        ans ^= num
     }
+
     return ans
     
 };
