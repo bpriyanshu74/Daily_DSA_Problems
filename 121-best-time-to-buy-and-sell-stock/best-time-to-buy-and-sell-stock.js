3a6c4d1ef59710319dcd9268acf508d72ea00f16
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-04 17:37:00 +0530

    Time: 6 ms (15.92%) | Memory: 65.9 MB (13.39%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,22 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let n = prices.length
-    let sell = new Array(n).fill(0)
+    let profit = 0, minprice = Infinity
 
-    let maxm = prices[n-1]
-
-    for(let i=n-1; i>=0; i--){
-        maxm = Math.max(maxm, prices[i])
-        sell[i] = maxm
+    for(let price of prices){
+        minprice = Math.min(price, minprice)
+        profit = Math.max(profit, price - minprice)
     }
 
-    let ans = 0
-
-    for(let i=0; i<n; i++){
-        ans = Math.max(ans, sell[i]-prices[i])
-    }
-
-    return ans
+    return profit
     
 };
