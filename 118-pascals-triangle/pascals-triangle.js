62295844f0224333e04fd115a23648c03b40b34d
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-06 00:26:42 +0530

    Time: 0 ms (100.00%) | Memory: 56 MB (13.70%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.js
+++ 118-pascals-triangle/pascals-triangle.js
@@ -3,19 +3,24 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    let ans = [[1]]
-    if(numRows == 1) return ans
-    for(let i=1; i<numRows; i++){
-        let lastrow = ans[ans.length-1]
-        let temp = [0, ...lastrow, 0]
-        let newrow = []
-        for(let j=1; j<temp.length; j++){
-            newrow.push(temp[j] + temp[j-1])
-        }
-
-        ans.push(newrow)
+    if (numRows === 0) {
+        return [];
+    }
+    if (numRows === 1) {
+        return [[1]];
     }
 
-    return ans
+    let prevRows = generate(numRows - 1);
+    let prevRow = prevRows[prevRows.length - 1];
+    let currentRow = [1];
+
+    for (let i = 1; i < numRows - 1; i++) {
+        currentRow.push(prevRow[i - 1] + prevRow[i]);
+    }
+
+    currentRow.push(1);
+    prevRows.push(currentRow);
+
+    return prevRows;
     
 };
