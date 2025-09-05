db10c72cb50df050f015f499ab8b6cb05a113fb4
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-05 21:32:21 +0530

    Time: 5 ms (31.78%) | Memory: 59.6 MB (33.42%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -3,23 +3,52 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    let zero_rows = new Set(), zero_cols = new Set()
-    for(let i=0; i< matrix.length; i++){
-        for(let j=0; j< matrix[0].length; j++){
+    // checking whether colzero or rowzero possible
+    let colzero = false
+    for(let row=0; row < matrix.length; row++){
+        if(matrix[row][0] == 0){
+            colzero = true
+            break
+        }
+    }
+
+    let rowzero = false
+    for(let col = 0; col < matrix[0].length; col++){
+        if(matrix[0][col] == 0){
+            rowzero = true
+            break
+        }
+    }
+    // setting up first row and first col
+
+    for(let i=1; i<matrix.length; i++){
+        for(let j=1; j<matrix[0].length; j++){
             if(matrix[i][j] == 0){
-                zero_rows.add(i)
-                zero_cols.add(j)
+                matrix[i][0] = 0
+                matrix[0][j] = 0
             }
         }
     }
 
-    for(let i=0; i<matrix.length; i++){
-        for(let j=0; j< matrix[0].length; j++){
-            if(zero_rows.has(i) || zero_cols.has(j)){
+    for(let i=1; i<matrix.length; i++){
+        for(let j=1; j<matrix[0].length; j++){
+            if(matrix[i][0] == 0 || matrix[0][j] == 0){
                 matrix[i][j] = 0
             }
         }
     }
+
+    if(colzero){
+        for(let row=0; row < matrix.length; row++){
+            matrix[row][0] = 0
+        }
+    }
+    if(rowzero){
+        for(let col = 0; col < matrix[0].length; col++){
+            matrix[0][col] = 0
+        }
+    }
+
     return matrix
     
 };
