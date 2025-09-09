451c5596542a3de40483a6a531a9ad49664540be
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-09 17:06:25 +0530

    Time: 15 ms (76.19%) | Memory: 67.2 MB (87.19%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.js
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.js
@@ -3,18 +3,23 @@
  * @return {number}
  */
 var largestRectangleArea = function(heights) {
-    // adding a sentinal value
-    heights.push(0)
-    let stack = [-1], maxarea = 0
+    let maxarea = 0, stack = [], n = heights.length
 
-    for(let i=0; i<heights.length; i++){
-        while(stack.length > 1 && heights[i] < heights[stack[stack.length-1]]){
-            let height = heights[stack.pop()]
-            let width = i - stack[stack.length-1] -1
-            maxarea = Math.max(maxarea, height*width)
+    for(let i=0; i<n; i++){
+        while(stack.length && heights[i] < heights[stack[stack.length-1]]){
+            let h = heights[stack.pop()]
+            let w = stack.length == 0 ? i : i - stack[stack.length-1] - 1
+            maxarea = Math.max(maxarea, h*w)
         }
         stack.push(i)
     }
+
+    while(stack.length){
+        let h = heights[stack.pop()]
+        let w = stack.length == 0 ? n : n - stack[stack.length-1] - 1
+        maxarea = Math.max(maxarea, h*w)
+    }
+
     return maxarea
     
 };
