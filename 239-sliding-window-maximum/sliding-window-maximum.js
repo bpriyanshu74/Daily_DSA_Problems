64cc5c874cb291c3cc215e8798d0de80ab5f517f
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-09 18:27:04 +0530

    Time: 42 ms (76.12%) | Memory: 85.4 MB (80.92%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.js
+++ 239-sliding-window-maximum/sliding-window-maximum.js
@@ -4,20 +4,25 @@
  * @return {number[]}
  */
 var maxSlidingWindow = function(nums, k) {
-    let q = [], output = []
-    for(let i=0; i < nums.length; i++){
-        while(q && nums[q[q.length-1]] <= nums[i]){
-            q.pop()
-        }
-        q.push(i)
+    let result = [], dq = []
 
-        if(q[0] == i-k){
-            q.shift()
+    for(let i=0; i<nums.length; i++){
+        // removal of left most index
+        if(dq.length && dq[0] <= i-k){
+            dq.shift()
         }
-        if(i >= k-1){
-            output.push(nums[q[0]])
+
+        // removal from top
+
+        while(dq.length && nums[i] >= nums[dq[dq.length-1]]){
+            dq.pop()
+        }
+
+        dq.push(i)
+
+        if(i >= k - 1){
+            result.push(nums[dq[0]])
         }
     }
-    return output
-    
+    return result
 };
