fca7f286425e794360597fcd829ed1321d12286d
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-10 13:20:11 +0530

    Time: 8 ms (76.93%) | Memory: 66.1 MB (31.73%) - LeetSync

--- 155-min-stack/min-stack.js
+++ 155-min-stack/min-stack.js
@@ -1,42 +1,55 @@
-class MinStack{
-    constructor(){
-        this.stack = []
-        this.minstack = []
+
+var MinStack = function() {
+    this.stack = []
+    this.minstack = []
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    let minval
+    if(this.minstack.length > 0){
+        minval = Math.min(this.minstack[this.minstack.length-1], val)
+    }
+    else{
+        minval = val
     }
 
-    push(val){
-        let minval
-        if(this.stack.length){
-            minval = Math.min(this.minstack[this.minstack.length-1], val)
-        }
-        else{
-            minval = val
-        }
+    this.stack.push(val)
+    this.minstack.push(minval)
+    
+};
 
-        this.stack.push(val)
-        this.minstack.push(minval)
-    }
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop()
+    this.minstack.pop()
+};
 
-    pop(){
-        if(this.stack.length){
-            let val = this.stack.pop()
-            this.minstack.pop()
-            return val
-        }
-        return null
-    }
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length-1]
+};
 
-    top(){
-        if(this.stack.length){
-            return this.stack[this.stack.length-1]
-        }
-        return null
-    }
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.minstack[this.minstack.length-1]
+    
+};
 
-    getMin(){
-        if(this.minstack.length){
-            return this.minstack[this.minstack.length-1]
-        }
-        return null
-    }
-}
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
