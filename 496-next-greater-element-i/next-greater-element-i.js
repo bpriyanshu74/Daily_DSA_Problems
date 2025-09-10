95b56b832a9b074291f9abe3d78cdbe521c3b0c7
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-10 13:37:30 +0530

    Time: 0 ms (100.00%) | Memory: 58.2 MB (24.34%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,20 +4,22 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    // using the monotonic stack
     let hashmap = new Map(), stack = []
 
-    for(let ch of nums2){
-        while(stack.length && ch > stack[stack.length-1]){
-            hashmap.set(stack.pop(), ch)
+    for(let num of nums2){
+        while(stack.length && num > stack[stack.length-1]){
+            hashmap.set(stack.pop(), num)
         }
-        stack.push(ch)
+        stack.push(num)
     }
 
     while(stack.length){
         hashmap.set(stack.pop(), -1)
     }
 
-    return nums1.map(num => hashmap.get(num))
-    
+    for(let i=0; i<nums1.length; i++){
+        nums1[i] = hashmap.get(nums1[i])
+    }
+
+    return nums1
 };
