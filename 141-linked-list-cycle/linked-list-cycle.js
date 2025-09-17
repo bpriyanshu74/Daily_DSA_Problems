19db6999fcb20cc750309e9b71986de04dd6de9d
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-17 14:07:21 +0530

    Time: 63 ms (12.17%) | Memory: 60.2 MB (6.45%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -12,11 +12,11 @@
  */
 var hasCycle = function(head) {
     let slow = head, fast = head
+
     while(fast && fast.next){
         slow = slow.next
         fast = fast.next.next
         if(slow == fast) return true
-       
     }
     return false
     
