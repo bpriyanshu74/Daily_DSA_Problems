fe8fe53d50dca969099f323e90c76387fe980389
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-18 22:17:31 +0530

    Time: 17 ms (94.34%) | Memory: 76.6 MB (97.76%) - LeetSync

--- 148-sort-list/sort-list.js
+++ 148-sort-list/sort-list.js
@@ -10,57 +10,22 @@
  * @return {ListNode}
  */
 var sortList = function(head) {
+    let arr = []
+    let temp = head
 
-    function findMiddle(head){
-        let slow = head, fast = head.next
+    while(temp){
+        arr.push(temp.val)
+        temp = temp.next
+    }
+    arr.sort((a,b) => a-b)
 
-        while(fast && fast.next){
-            slow = slow.next
-            fast = fast.next.next
-        }
+    temp = head
 
-        return slow
+    for(let i=0; i<arr.length; i++){
+        temp.val = arr[i]
+        temp = temp.next
     }
 
-    function merge(a,b){
-        let dummy = new ListNode(-100000)
-
-        let temp = dummy
-
-        while(a && b){
-            if(a.val <= b.val){
-                temp.next = a
-               
-                a = a.next
-            }
-            else{
-                temp.next = b
-            
-                b = b.next
-            }
-            temp = temp.next
-        }
-
-        if(a){
-            temp.next = a
-        }
-        if(b){
-            temp.next = b
-        }
-        return dummy.next
-    }
-
-    if(!head || !head.next) return head
-
-    let middle = findMiddle(head)
-
-    let right = middle.next
-    middle.next = null
-    let left = head
-
-    let lefthead = sortList(left)
-    let righthead = sortList(right)
-
-    return merge(lefthead,righthead)
+    return head
     
 };
