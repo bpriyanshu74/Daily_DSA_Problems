a6ca7fa2393263f099ae372b54c148dc00f6c89b
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-21 17:32:43 +0530

    Time: 12 ms (30.69%) | Memory: 61.7 MB (10.40%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.js
+++ 1036-rotting-oranges/rotting-oranges.js
@@ -3,33 +3,29 @@
  * @return {number}
  */
 var orangesRotting = function(grid) {
-    let fresh = 0, maxtime = 0, q = []
+    let q = [], fresh = 0, maxtime = 0, rows = grid.length, cols = grid[0].length
     let dir = [[1,0], [0,1], [-1,0], [0,-1]]
 
-    for(let i=0; i<grid.length; i++){
-        for(let j=0; j<grid[0].length; j++){
-            if(grid[i][j] == 2){
-                q.push([i,j,maxtime])
-            }
-            if(grid[i][j] == 1){
-                fresh++
-            }
+    // creating queue of rotten oranges for multisource bfs
+    for(let i=0; i<rows; i++){
+        for(let j=0; j<cols; j++){
+            if(grid[i][j] == 1) fresh++
+            if(grid[i][j] == 2) q.push([i,j,0])
         }
     }
 
     while(q.length){
-        let [x,y,time] = q.shift()
-        maxtime = Math.max(maxtime, time)
+        let [x,y,t] = q.shift()
+        maxtime = Math.max(maxtime, t)
         for(let [dx,dy] of dir){
-            let nx = dx+x, ny = dy+y
-            if(nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[0].length && grid[nx][ny] == 1){
+            let nx = x + dx, ny = y + dy
+            if(nx >= 0 && nx < rows && ny >= 0 && ny < cols && grid[nx][ny]==1){
                 grid[nx][ny] = 2
+                q.push([nx,ny,t+1])
                 fresh--
-                q.push([nx,ny, time+1])
             }
-        }
+        } 
     }
-
-    return fresh ? -1 : maxtime
     
+    return fresh > 0 ? -1 : maxtime
 };
