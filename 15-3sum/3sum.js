f6221c66380ea1207f872c3cf18c0cb96eb545e5
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-25 13:10:18 +0530

    Time: 38 ms (46.72%) | Memory: 72.4 MB (57.96%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,18 +3,18 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let ans = new Set()
+    let ans = []
     nums.sort((a,b) => a-b)
-
     for(let i=0; i<nums.length-2; i++){
+        if(i > 0 && nums[i] == nums[i-1]) continue
         let l = i+1, r = nums.length-1
+
         while(l < r){
             let sum = nums[i] + nums[l] + nums[r]
             if(sum == 0){
-                let triplet = [nums[i], nums[l], nums[r]].toString()
-                if(!ans.has(triplet)){
-                    ans.add(triplet)
-                }
+                ans.push([nums[i], nums[l], nums[r]])
+                while(l < r && nums[l] == nums[l+1])l++
+                while(l < r && nums[r] == nums[r-1]) r--
                 l++
                 r--
             }else if(sum < 0){
@@ -24,6 +24,7 @@ var threeSum = function(nums) {
             }
         }
     }
-    return Array.from(ans).map(str => str.split(',').map(Number))
+
+    return ans
     
 };
