e7805ffcc0f5355dd5974fc707163a7d94a9887f
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-25 11:23:55 +0530

    Time: 2 ms (63.15%) | Memory: 56.2 MB (43.34%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -4,15 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    // two pointe approach
+    let l=0, r = numbers.length-1
 
-    let l=0; r=numbers.length-1
-    while(l<r){
-        if(numbers[l] + numbers[r] == target) return [l+1, r+1]
-        else if(numbers[l] + numbers[r] > target) r--
+    while(l < r){
+        let twosum = numbers[l] + numbers[r]
+
+        if(twosum == target) return [l+1,r+1]
+        else if(twosum > target) r--
         else l++
     }
 
-    
+    return [-1,-1]
     
 };
