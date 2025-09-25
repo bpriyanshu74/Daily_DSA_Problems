2bd917840a7d04d932450f71a252b7c8d1674b75
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-25 11:43:18 +0530

    Time: 0 ms (100.00%) | Memory: 61 MB (14.53%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,23 +3,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let stk = []
+    let ans = new Array(nums.length).fill(0), k=0
 
-    for(let i=nums.length-1; i>= 0; i--){
+    for(let i=0; i<nums.length; i++){
         if(nums[i] != 0){
-            stk.push(nums[i])
+            ans[k] = nums[i]
+            k++
         }
     }
-    let i=0
-    while(stk.length){
-        nums[i] = stk.pop()
-        i++
-    }
-    while(i < nums.length){
-        nums[i] = 0
-        i++
+
+    for(let i=0; i<nums.length; i++){
+        nums[i] = ans[i]
     }
 
-    return nums
+    return ans
     
 };
