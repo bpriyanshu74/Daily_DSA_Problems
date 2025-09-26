8fc041463055a402ec17da2d0d798aa2a733fe59
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-26 14:45:26 +0530

    Time: 17 ms (55.90%) | Memory: 58 MB (53.73%) - LeetSync

--- 18-4sum/4sum.js
+++ 18-4sum/4sum.js
@@ -4,39 +4,31 @@
  * @return {number[][]}
  */
 var fourSum = function(nums, target) {
-    // brute force
-    if(nums.length < 4) return []
-    let res = []
-
+    let ans = []
     nums.sort((a,b) => a-b)
+
     for(let i=0; i<nums.length-3; i++){
         if(i > 0 && nums[i] == nums[i-1]) continue
         for(let j=i+1; j<nums.length-2; j++){
-            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
-            let newtarget = target - (nums[i] + nums[j])
-
+            if(j > i+1 && nums[j] == nums[j-1]) continue
             let l = j+1, r = nums.length-1
 
             while(l < r){
-                let sum = nums[l] + nums[r]
-                if(sum == newtarget){
-                    res.push([nums[i], nums[j], nums[l], nums[r]])
+                let sum = nums[i] + nums[j] + nums[l] + nums[r]
+                if(sum == target){
+                    let quad = [nums[i], nums[j], nums[l], nums[r]]
+                    ans.push(quad)
                     while(l < r && nums[l] == nums[l+1]) l++
                     while(l < r && nums[r] == nums[r-1]) r--
-
                     l++
                     r--
-                }
-                else if(sum < newtarget){
+                }else if(sum < target){
                     l++
-                }
-                else{
+                }else{
                     r--
                 }
             }
         }
     }
-
-    return res
-
+    return ans
 };
