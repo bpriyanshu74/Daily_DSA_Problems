e1b8603a6fdc9c9b13173224c70d399afa85c93d
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-27 01:40:07 +0530

    Time: 10 ms (91.32%) | Memory: 59.2 MB (24.93%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -4,32 +4,23 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
+    let maxcount = 0, l = 0
+    let freq = new Array(26).fill(0), maxfreq = -1
 
-    function calculatemax(arr){
-        let maxm = 0
-        for(let i=0; i<arr.length; i++){
-            maxm = Math.max(maxm, arr[i])
-        }
-        return maxm
-    }
-    let maxl = 0,l=0,r=0, freq_arr = new Array(26).fill(0), maxf = 0
-    while(r < s.length){
-        let characterIndex = s.charCodeAt(r) - 'A'.charCodeAt(0)
-        freq_arr[characterIndex]++ 
-        maxf = Math.max(maxf, freq_arr[characterIndex])
-        let changes = (r-l+1) - maxf
+    for(let r = 0; r < s.length; r++){
+        let index = s[r].charCodeAt(0) - 65
+        freq[index]++
+        maxfreq = Math.max(maxfreq, freq[index])
 
-        if(changes > k){
-            let leftcharindex = s.charCodeAt(l) - 'A'.charCodeAt(0)
-            freq_arr[leftcharindex]--
-            maxf = calculatemax(freq_arr)
+        let need = (r-l+1) - maxfreq
+
+        if(need > k){
+            let lindex = s[l].charCodeAt(0) - 65
+            freq[lindex]--
             l++
+        }else{
+            maxcount = Math.max(maxcount, r-l+1)
         }
-        if(changes <= k){
-            maxl = Math.max(r-l+1, maxl)
-        }
-        r++
     }
-    return maxl  
-    
+    return maxcount
 };
