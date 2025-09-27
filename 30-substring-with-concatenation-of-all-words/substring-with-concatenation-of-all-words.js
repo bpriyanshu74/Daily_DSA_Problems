f9a5e71458be56e60fada2e9fa0081e78bd22f7d
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-27 16:13:26 +0530

    Time: 27 ms (66.46%) | Memory: 65.9 MB (10.56%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.js
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.js
@@ -4,36 +4,41 @@
  * @return {number[]}
  */
 var findSubstring = function(s, words) {
-    let n = s.length, m = words.length, k = words[0].length
-let wlen = m * k
-let ans = []
+    let n = s.length, m = words.length, wordlen = words[0].length, ans = []
 
-// setting up frequency of word in words
-let f1 = {}
-for (let ch of words) {
-    f1[ch] = (f1[ch] || 0) + 1
-}
-
-function check(a, b) {
-    for (let key in a) {
-        if (a[key] !== b[key]) return false
+    let f1 = {}
+    for(let ch of words){
+        f1[ch] = (f1[ch] || 0) + 1
     }
-    return true
-}
 
-// iterating over the string s 
-for (let i = 0; i <= n - wlen; i++) {
-    let f2 = {}, temp = ''
-    for (let j = i; j < i + wlen; j++) {
-        temp += s[j]
-        if ((j - i + 1) % k === 0) {
-            f2[temp] = (f2[temp] || 0) + 1
-            temp = ''
+    // trying all offsets
+    for(let offset = 0; offset < wordlen; offset++){
+
+        let l = offset, f2 = {}, count = 0
+
+        for(let right = offset; right+wordlen <= s.length; right += wordlen){
+            let curword = s.slice(right, right+wordlen)
+
+            if(f1[curword]){
+                f2[curword] = (f2[curword] || 0) + 1
+                count++
+
+                while(f2[curword] > f1[curword]){
+                    let leftword = s.slice(l, l+wordlen)
+                    f2[leftword]--
+                    count--
+                    l += wordlen
+                }
+
+                if(count == m){
+                    ans.push(l)
+                }
+            }else{
+                f2 = {}
+                count = 0
+                l = right+wordlen
+            }
         }
     }
-    if (check(f1, f2)) {
-        ans.push(i)
-    }
-}
     return ans
 };
