4fe521385866769851205eedc96bf44cb64b1b44
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-09-29 12:35:53 +0530

    Time: 2273 ms (16.09%) | Memory: 63.4 MB (57.80%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.js
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.js
@@ -4,41 +4,34 @@
  * @return {number[]}
  */
 var findSubstring = function(s, words) {
-    let n = s.length, m = words.length, wordlen = words[0].length, ans = []
+    let f1 = {}, n = s.length, m = words.length, k = words[0].length, total_len = m*k
+    let res = []
 
-    let f1 = {}
     for(let ch of words){
         f1[ch] = (f1[ch] || 0) + 1
     }
 
-    // trying all offsets
-    for(let offset = 0; offset < wordlen; offset++){
+    //  for every offset
+    for(let i=0; i<= n-total_len; i++){
+        let valid = true
+        let f2 = {}
+        for(let j=0; j<m; j++){
+            let start = i+j*k
+            let word = s.slice(start, start+k)
 
-        let l = offset, f2 = {}, count = 0
-
-        for(let right = offset; right+wordlen <= s.length; right += wordlen){
-            let curword = s.slice(right, right+wordlen)
-
-            if(f1[curword]){
-                f2[curword] = (f2[curword] || 0) + 1
-                count++
-
-                while(f2[curword] > f1[curword]){
-                    let leftword = s.slice(l, l+wordlen)
-                    f2[leftword]--
-                    count--
-                    l += wordlen
-                }
-
-                if(count == m){
-                    ans.push(l)
-                }
-            }else{
-                f2 = {}
-                count = 0
-                l = right+wordlen
+            if(!(word in f1)){
+                valid = false
+                break
+            }
+            f2[word] = (f2[word] || 0) + 1
+            if(f2[word] > f1[word]){
+                valid = false
+                break
             }
         }
+        if(valid){
+            res.push(i)
+        }
     }
-    return ans
+    return res
 };
