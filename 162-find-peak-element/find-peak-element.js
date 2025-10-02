b6b270ae54e2d3f1a4068972eb3871a0a3261ddb
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-02 11:35:28 +0530

    Time: 1 ms (7.54%) | Memory: 57.3 MB (6.11%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -3,19 +3,12 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    if(nums.length == 1) return 0
-    if(nums[0] > nums[1]) return 0
-    if(nums[nums.length - 1] > nums[nums.length-2]) return nums.length - 1
-    let l=0, r = nums.length-1
-    while(l <= r){
-        let mid = l + Math.floor((r-l)/2)
-        if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid
-        if(nums[mid] > nums[mid+1]){
-            r = mid -1
-        }else{
-            l = mid + 1
-        }
+    if(nums.length <= 1) return 0
+    let newnums = [-Infinity,...nums, -Infinity]
+    for(let i=1; i<newnums.length-1; i++){
+        if(newnums[i] > newnums[i-1] && newnums[i] > newnums[i+1]) return i-1
     }
-    return -1
+
+
     
 };
