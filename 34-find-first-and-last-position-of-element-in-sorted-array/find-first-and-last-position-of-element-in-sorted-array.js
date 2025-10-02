31df7ca0291051c0456767a0016699942598becd
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-02 10:27:17 +0530

    Time: 2 ms (10.81%) | Memory: 54.5 MB (72.41%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,18 +4,16 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    let pos = [100000, -1]
-    
+    let first = -1, last = -1
+
     for(let i=0; i<nums.length; i++){
         if(nums[i] == target){
-            pos[0] = Math.min(pos[0], i)
-            pos[1] = Math.max(pos[1], i)
+            if(first == -1) first = i
+            last = i
         }
     }
 
-    if(pos[0] == 100000) return [-1,-1]
-
-    return pos
+    return [first, last]
 
 
 };
