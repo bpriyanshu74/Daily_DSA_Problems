0a6a885a3acb5477cb5eb1b158632c4346677b73
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-02 10:24:08 +0530

    Time: 2 ms (10.81%) | Memory: 55.6 MB (16.51%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,26 +4,16 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    let pos = [-1, -1]
-    let l = 0, r = nums.length-1
-
-    while(l <= r){
-        if(pos[0] == -1){
-            if(nums[l] == target){
-                pos[0] = l
-            }else{
-                l++
-            }
-        }else if(pos[1] == -1){
-            if(nums[r] == target){
-                pos[1] = r
-            }else{
-                r--
-            }
-        }else{
-            break
+    let pos = [100000, -1]
+    
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] == target){
+            pos[0] = Math.min(pos[0], i)
+            pos[1] = Math.max(pos[1], i)
         }
     }
+
+    if(pos[0] == 100000) return [-1,-1]
 
     return pos
 
