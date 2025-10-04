b06a3d42ae50d54f610000bb644b9c6f841a75ca
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-04 22:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (39.70%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -4,28 +4,10 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let l =0, r = nums.length - 1
-    while(l <= r){
-        let mid = l + Math.floor((r-l)/2)
-        if(nums[mid] == target) return mid
-        if(nums[mid] >= nums[l]){
-            // left half is sorted
-            if(target >= nums[l] && target < nums[mid]){
-                r = mid - 1
-            }
-            else{
-                l = mid + 1
-            }
-        }
-        else{
-            // right half is sorted
-            if(target > nums[mid] && target <= nums[r]){
-                l = mid + 1
-            }
-            else{
-                r = mid - 1
-            }
-        }
+    // linear search
+
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] == target) return i
     }
     return -1
     
