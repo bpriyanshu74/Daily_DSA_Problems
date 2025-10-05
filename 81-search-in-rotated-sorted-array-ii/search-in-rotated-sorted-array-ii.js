ac44a257eedee55f766599c99d26d24c57ad28bc
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-05 11:37:11 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (19.45%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
@@ -10,22 +10,20 @@ var search = function(nums, target) {
         let mid = l + Math.floor((r-l)/2)
 
         if(nums[mid] == target) return true
-        if(nums[mid] == nums[r]){
-            r--
-            continue
-        }
-        if(nums[l] <= nums[mid]){
+        if(nums[mid] > nums[r]){
             if(target >= nums[l] && target <= nums[mid]){
                 r = mid - 1
             }else{
                 l = mid + 1
             }
-        }else{
+        }else if(nums[mid] < nums[r]){
             if(target >= nums[mid] && target <= nums[r]){
                 l = mid + 1
             }else{
                 r = mid - 1
             }
+        }else{
+            r--
         }
     }
     return false
