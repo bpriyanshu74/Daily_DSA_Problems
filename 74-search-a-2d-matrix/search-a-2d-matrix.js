216b6ee02f675174c01bd0f174235fd541ca5da9
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-05 12:30:26 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (15.93%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -4,12 +4,23 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    let n= matrix.length, m = matrix[0].length
 
-    for(let i=0; i<n; i++){
-        for(let j=0; j<m; j++){
-            if(matrix[i][j] == target) return true
+    for(let row of matrix){
+        let l = 0, r = row.length-1
+
+        while(l <= r){
+            let mid = l + Math.floor((r-l)/2)
+
+            if(row[mid] == target) return true
+            else if(row[mid] < target){
+                l = mid + 1
+            }else{
+                r = mid-1
+            }
         }
     }
+
     return false
+
+    
 };
