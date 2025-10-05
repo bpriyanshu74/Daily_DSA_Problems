a6a3c3946ddd73924cb10e15a0f71435d14e6a6b
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-05 16:03:18 +0530

    Time: 4 ms (25.98%) | Memory: 62.7 MB (24.84%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.js
+++ 658-find-k-closest-elements/find-k-closest-elements.js
@@ -5,14 +5,32 @@
  * @return {number[]}
  */
 var findClosestElements = function(arr, k, x) {
-    let l = 0, r = arr.length-1
+    let n = arr.length
+    // general cases
+    if(x < arr[0]) return arr.slice(0, k)
+    if(x >= arr[arr.length-1]) return arr.slice(arr.length-k)
+    if(arr.length == k) return arr
 
-    while(r-l >= k){
-        if(Math.abs(arr[l]-x) > Math.abs(arr[r]-x)){
-            l++
-        }else{
-            r--
+    let l=0, r = arr.length
+    while(l < r){
+        let mid = l + Math.floor((r-l)/2)
+
+        if(arr[mid] < x) l = mid + 1
+        else r = mid
+    }
+
+    let left = l, right = l
+
+    while (right - left - 1 < k) {
+        if (left < 0) {
+            right++;
+        } else if (right >= n) {
+            left--;
+        } else if (Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
+            left--;
+        } else {
+            right++;
         }
     }
-    return arr.slice(l, l+k)
+    return arr.slice(left+1, right)
 };
