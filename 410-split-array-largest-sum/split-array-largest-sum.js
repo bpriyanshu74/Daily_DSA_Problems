3bfcfe628b1dac7f93bc98f3103c81eddc875631
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-06 10:04:30 +0530

    Time: 1 ms (85.60%) | Memory: 53.3 MB (96.60%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.js
+++ 410-split-array-largest-sum/split-array-largest-sum.js
@@ -5,36 +5,36 @@
  */
 var splitArray = function(nums, k) {
 
-    function check_split(i, nums){
-        let splits = 1, cur_sum = 0
+    function check_split(sum, nums){
+        let split = 1, cursum = 0
         for(let j=0; j<nums.length; j++){
-            if(cur_sum + nums[j] <= i){
-                cur_sum += nums[j]
-            }
-            else{
-                cur_sum = nums[j]
-                splits++
+            if(cursum + nums[j] <= sum){
+                cursum += nums[j]
+            }else{
+                cursum = nums[j]
+                split++
             }
         }
-        return splits
+        return split
     }
 
-    let l = Math.max(...nums)
-    let r = nums.reduce((a,b) => a+b, 0)
-    let ans = -1
+    // so we now answer will lie in the range Max(nums) && Max(sum(nums))
 
-    if(k > nums.length) return -1
+    let l = Math.max(...nums)
+    let r = 0
+    for(let num of nums){
+        r += num
+    }
+    // so we have range l and r
 
     while(l <= r){
         let mid = l + Math.floor((r-l)/2)
-        if(check_split(mid, nums) <= k){
+        if(check_split(mid,nums) <= k){
             ans = mid
-            r = mid -1
-        }
-        else{
+            r = mid - 1
+        }else{
             l = mid + 1
         }
     }
     return ans
-    
 };
