67b74562a6e428db82cde247957482adba750d8f
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-06 12:09:56 +0530

    Time: 21 ms (52.21%) | Memory: 66.9 MB (16.58%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.js
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.js
@@ -5,35 +5,31 @@
  * @return {number}
  */
 var minDays = function(bloomDay, m, k) {
-    if(bloomDay.length < m*k) return -1
 
-    function makePossible(day, arr, boq, flow){
-        let newboq = 0, count = 0
-        for(let i=0; i<arr.length; i++){
-            if(arr[i] <= day){
-                count++
-            }
-            else{
-                newboq += Math.floor(count / flow)
-                count = 0
+    function check(maxdays){
+        let fcount = 0, boq = 0
+        for(let bd of bloomDay){
+            if(bd <= maxdays){
+                fcount++
+            }else{
+                boq += Math.floor(fcount/k)
+                fcount = 0
             }
         }
-        newboq += Math.floor(count / flow)
-        
-        return newboq >= boq
+        boq += Math.floor(fcount/k)
+        return boq >= m
     }
-   
-    let l = Math.min(...bloomDay), r = Math.max(...bloomDay), ans = r
+    let l = Math.min(...bloomDay), r = Math.max(...bloomDay), ans = -1
+
     while(l <= r){
         let mid = l + Math.floor((r-l)/2)
-        if(makePossible(mid, bloomDay, m, k)){
+
+        if(check(mid)){
             ans = mid
             r = mid - 1
-        }
-        else{
+        }else{
             l = mid + 1
         }
     }
     return ans
-    
 };
