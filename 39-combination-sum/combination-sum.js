064a2db423a52745974135b5b88ad8840c07ec44
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-07 20:28:31 +0530

    Time: 1 ms (97.18%) | Memory: 60 MB (47.88%) - LeetSync

--- 39-combination-sum/combination-sum.js
+++ 39-combination-sum/combination-sum.js
@@ -5,22 +5,21 @@
  */
 var combinationSum = function(candidates, target) {
     let res = []
-    function dfs(path, cursum, index){
-        if(index === candidates.length){
-            if(cursum == target){
-                res.push([...path])
-                return
-            }
+    function dfs(path, sum, index){
+        if(sum == 0){
+            res.push([...path])
             return
         }
-        if(cursum > target) return
-        path.push(candidates[index])
-        dfs(path, cursum + candidates[index], index)
+        if(sum < 0) return 
 
-        path.pop()
-        dfs(path, cursum, index+1)
+        for(let i=index; i<candidates.length; i++){
+            path.push(candidates[i])
+            dfs(path, sum - candidates[i], i)
+            path.pop()
+        }
     }
-    dfs([],0, 0)
+
+    dfs([], target, 0)
+
     return res
-    
 };
