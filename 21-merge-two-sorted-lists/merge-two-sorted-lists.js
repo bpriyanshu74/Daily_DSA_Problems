696ebda71c66fac6e2ae760e55f52213710aaa4f
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-07 17:12:19 +0530

    Time: 1 ms (38.23%) | Memory: 57.8 MB (43.52%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -11,39 +11,15 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
+    
     if(!list1) return list2
     if(!list2) return list1
 
-    let t1 = list1, t2 = list2, cur
-    let dummy = new ListNode(-1)
-
-    if(t1.val <= t2.val){
-        dummy.next = t1
-        cur = t1
-        t1 = t1.next
+    if(list1.val <= list2.val){
+        list1.next = mergeTwoLists(list1.next, list2)
+        return list1
     }else{
-        dummy.next = t2
-        cur = t2
-        t2 = t2.next
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2
     }
-
-    while(t1 && t2){
-        if(t1.val <= t2.val){
-            cur.next = t1
-            t1 = t1.next
-        }else{
-            cur.next = t2
-            t2 = t2.next
-        }
-        cur = cur.next
-    }
-
-    if(t1){
-        cur.next = t1
-    }
-    if(t2){
-        cur.next = t2
-    }
-
-    return dummy.next
 };
