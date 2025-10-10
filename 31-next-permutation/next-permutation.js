d28aaa353abf344392a52c25a6a52e6911c212ad
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-10 23:43:31 +0530

    Time: 1 ms (24.65%) | Memory: 57.3 MB (26.67%) - LeetSync

--- 31-next-permutation/next-permutation.js
+++ 31-next-permutation/next-permutation.js
@@ -3,7 +3,6 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function(nums) {
-    // finding the pivot
     let n = nums.length, i = n-2
 
     while(i >= 0 && nums[i] >= nums[i+1]){
@@ -15,16 +14,13 @@ var nextPermutation = function(nums) {
         while(nums[j] <= nums[i]){
             j--
         }
-        [nums[i], nums[j]] = [nums[j], nums[i]]
+        [nums[j], nums[i]] = [nums[i], nums[j]]
     }
-    // reversing the entire suffix
-    let l = i+1, r = n-1
+    let l = i+1, r =n-1
     while(l < r){
         [nums[l], nums[r]] = [nums[r], nums[l]]
         l++
         r--
     }
-
-    return nums
     
 };
