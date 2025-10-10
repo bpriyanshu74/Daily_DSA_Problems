ebfa995ad53df29b882fca758ac34220e8cc52b3
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-10 22:58:41 +0530

    Time: 0 ms (100.00%) | Memory: 57.7 MB (16.02%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.js
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.js
@@ -12,20 +12,27 @@
  */
 var zigzagLevelOrder = function(root) {
     if(!root) return []
-    let res = [], q = [root], index = 0
+
+    let res = []
+    let left_to_right = true
+    let q = [root]
+
     while(q.length){
-        let level = []
-        for(let i=q.length; i > 0; i--){
+        let level = [], size = q.length
+        for(let i=0; i < size; i++){
             let node = q.shift()
             level.push(node.val)
             if(node.left) q.push(node.left)
             if(node.right) q.push(node.right)
         }
-        if(index%2 == 0) res.push(level)
-        else{
+        if(left_to_right){
+            res.push(level)
+        }else{
             res.push(level.reverse())
-        } 
-        index++
+        }
+        left_to_right = !left_to_right
     }
+
     return res
+    
 };
