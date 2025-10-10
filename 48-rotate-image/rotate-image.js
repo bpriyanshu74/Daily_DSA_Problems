a341883321c4e53ff925b812ff8fba48e1f993d9
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-10 23:07:55 +0530

    Time: 1 ms (23.47%) | Memory: 56.6 MB (9.78%) - LeetSync

--- 48-rotate-image/rotate-image.js
+++ 48-rotate-image/rotate-image.js
@@ -3,21 +3,26 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    // two step -- transpose and reverse the rows
+    // doing transpose only on the upper half
 
     for(let i=0; i<matrix.length; i++){
-        for(let j=i+1; j<matrix[0].length; j++){
+        for(let j=i; j<matrix[0].length; j++){
             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
         }
     }
 
+    // now reverse each row
+
     for(let row of matrix){
         let l = 0, r = row.length-1
+
         while(l < r){
             [row[l], row[r]] = [row[r], row[l]]
             l++
             r--
         }
     }
+
     return matrix
+    
 };
