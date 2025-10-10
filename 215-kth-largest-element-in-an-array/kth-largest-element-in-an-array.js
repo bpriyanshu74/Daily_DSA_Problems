0cbbbf048078ed8a2a7e9fb48f52d0e2d8b82d70
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-10 20:34:39 +0530

    Time: 211 ms (5.04%) | Memory: 79 MB (5.04%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -4,7 +4,68 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    nums.sort((a,b) => a-b)
-    return nums[nums.length - k]
+    class Maxheap{
+        constructor(){
+            this.heap = []
+        }
+        insert(num){
+            this.heap.push(num)
+            this.percolate_up()
+        }
+        extract(){
+            let max = this.heap[0]
+            this.heap[0] = this.heap.pop()
+            this.percolate_down()
+            return max
+        }
+        swap(a,b){
+            [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]]
+        }
+
+        percolate_up(){
+            let index = this.heap.length-1
+            while(index > 0){
+                let parent = Math.floor((index-1)/2)
+                if(this.heap[parent] < this.heap[index]){
+                    this.swap(parent, index)
+                    index = parent
+                }else{
+                    break
+                }
+            }
+        }
+
+        percolate_down(){
+            let index = 0, largest = index, n = this.heap.length
+
+            while(true){
+                let left = 2*index+1
+                let right = 2*index+2
+
+                if(left < n && this.heap[left] > this.heap[largest]){
+                    largest = left
+                }
+                if(right < n && this.heap[right] > this.heap[largest]){
+                    largest = right
+                }
+
+                if(largest != index){
+                    this.swap(largest, index)
+                    index = largest
+                }else break
+            }
+        }
+    }
+
+    let maxheap = new Maxheap()
+    for(let num of nums){
+        maxheap.insert(num)
+    }
+    let res = 0
+    while(k > 0){
+        res = maxheap.extract()
+        k--
+    }
+    return res
     
 };
