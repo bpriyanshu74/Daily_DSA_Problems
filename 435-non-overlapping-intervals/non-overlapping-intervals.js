435606f2a3d215dde23f06458f75b20645e98b8d
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-10 13:59:24 +0530

    Time: 80 ms (67.85%) | Memory: 85.9 MB (82.68%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.js
+++ 435-non-overlapping-intervals/non-overlapping-intervals.js
@@ -3,18 +3,20 @@
  * @return {number}
  */
 var eraseOverlapIntervals = function(intervals) {
-    intervals.sort((a,b) => a[1]-b[1])
+    let count = 0
+    if(intervals.length <= 1) return count
 
-    let end = -Infinity, count = 0
+    intervals.sort((a,b) => a[1] - b[1])
 
-    for(let [s,e] of intervals){
-        if(s >= end){
-            end = e
-        }
-        else{
+    let [start, end] = intervals[0]
+
+    for(let i=1; i<intervals.length; i++){
+        let [cur_start, cur_end] = intervals[i]
+        if(end <= cur_start){
+            end = cur_end
+        }else{
             count++
         }
     }
     return count
-    
 };
