b3a9e6593e9db17b8f5d2e8f3f87b15b23c7f694
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-10 16:29:58 +0530

    Time: 4 ms (50.00%) | Memory: 59.8 MB (38.91%) - LeetSync

--- 47-permutations-ii/permutations-ii.js
+++ 47-permutations-ii/permutations-ii.js
@@ -3,30 +3,26 @@
  * @return {number[][]}
  */
 var permuteUnique = function(nums) {
-    
-    function dfs(ds){
-        if(ds.length == nums.length){
-            res.push([...ds])
+    function dfs(arr, path){
+        if(path.length == arr.length){
+            res.push([...path])
             return
         }
 
-        for(let i=0; i<nums.length; i++){
-            if(i > 0 && nums[i] == nums[i-1] && !visited[i-1]){
-                continue
-            }
+        for(let i=0; i<arr.length; i++){
             if(!visited[i]){
-                visited[i] = true
-                ds.push(nums[i])
-                dfs(ds)
-                visited[i] = false
-                ds.pop()
+                if(i > 0 && arr[i] == arr[i-1] && visited[i-1]) continue
+                path.push(arr[i])
+                visited[i] = 1
+                dfs(arr,path)
+                path.pop()
+                visited[i] = 0
             }
         }
     }
-
-
-    let visited = new Array(nums.length).fill(false), res = []
     nums.sort((a,b) => a-b)
-    dfs([])
+    let res = [], visited = new Array(nums.length).fill(0)
+    dfs(nums,[])
     return res
+    
 };
