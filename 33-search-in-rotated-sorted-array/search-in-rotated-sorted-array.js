ce754042a983c69195c54575f3cd2d4594580dcf
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-10 23:22:53 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (17.01%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -4,25 +4,28 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let l = 0, r = nums.length-1
+    let l = 0, r = nums.length-1, ans = -1
 
-    while( l <= r){
+    while(l <= r){
         let mid = l + Math.floor((r-l)/2)
-
-        if(nums[mid] == target) return mid
-        else if(nums[l] <= nums[mid]){
-            if(target >= nums[l] && target <= nums[mid]){
-                r = mid-1
-            }else{
-                l = mid + 1
-            }
+        // element found
+        if(nums[mid] == target){
+            ans = mid
+            break
+        }
+        //first half
+        if(nums[mid] >= nums[l]){
+            if(target >= nums[l] && target < nums[mid]) r = mid-1
+            else l = mid + 1
         }else{
-            if(target >= nums[mid] && target <= nums[r]){
-                l = mid + 1
-            }else{
-                r = mid - 1
-            }
+            if((target > nums[mid] && target <= nums[r])) l = mid + 1
+            else r = mid - 1
         }
     }
+    if(nums[ans] == target){
+        return ans
+    }
+
     return -1
+    
 };
