240c021aac88cc0475cb3839c5301ad90e79f6c6
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-11 12:07:21 +0530

    Time: 35 ms (32.63%) | Memory: 67.1 MB (5.97%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -10,40 +10,78 @@
  * @return {ListNode}
  */
 var mergeKLists = function(lists) {
-    if(lists.length == 0) return null
-    if(lists.length == 1) return lists[0] 
-
-    function merge(a,b){
-        let dummy = new ListNode(-1), temp = dummy
-        while(a && b){
-            if(a.val <= b.val){
-                temp.next = a 
-                a = a.next
+    class Minheap{
+        constructor(){
+            this.heap = []
+            this.size = 0
+        }
+        insert(node){
+            this.heap.push(node)
+            this.percolate_up()
+            this.size++
+        }
+        extract_min(){
+            if(this.heap.length === 0) return null;
+            let min = this.heap[0];
+            let last = this.heap.pop();
+            this.size--;
+            if(this.heap.length > 0){
+                this.heap[0] = last;
+                this.percolate_down();
             }
-            else{
-                temp.next = b
-                b = b.next
+            return min;
+        }
+        swap(a,b){
+            [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]]
+        }
+        percolate_up(){
+            let index = this.heap.length-1
+            while(index > 0){
+                let parent = Math.floor((index-1)/2)
+                if(this.heap[index].val < this.heap[parent].val){
+                    this.swap(parent, index)
+                    index = parent
+                }else{
+                    break
+                }
             }
-            temp = temp.next
         }
-        while(a){
-            temp.next = a
-            a = a.next
-            temp = temp.next
+        percolate_down(){
+            let index = 0, smallest = index, n = this.heap.length
+            while(true){
+                let left = 2 * index + 1
+                let right = 2 * index + 2
+                if(left < n && this.heap[left].val < this.heap[smallest].val){
+                    smallest = left
+                }
+                if(right < n && this.heap[right].val < this.heap[smallest].val){
+                    smallest = right
+                }
+                if(smallest != index){
+                    this.swap(smallest, index)
+                    index = smallest
+                }else{
+                    break
+                }
+            }
         }
-        while(b){
-            temp.next = b
-            b = b.next
-            temp = temp.next
-        }
-        return dummy.next
     }
+    let dummy = new ListNode(-1), temp = dummy
+    let heap = new Minheap()
 
-    while(lists.length > 1){
-        let newll = merge(lists.pop(), lists.pop())
-        lists.push(newll)
+    for(let list of lists){
+        if(list) heap.insert(list)
     }
+    // now we have min heap of linked list heads
 
-    return lists[0]
-    
+    while(heap.size){
+        let node = heap.extract_min()
+        // setting up in the new ll
+        temp.next = node
+        temp = temp.next
+        // inserting the new value of pooped node
+        node = node.next || null
+        if(node) heap.insert(node)
+    }
+    return dummy.next
 };
