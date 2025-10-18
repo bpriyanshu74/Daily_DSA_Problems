2b8431d4ad41646c8d05806dc463112a3583cd72
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-18 22:09:45 +0530

    Time: 20 ms (5.19%) | Memory: 61.9 MB (5.07%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.js
+++ 547-number-of-provinces/number-of-provinces.js
@@ -3,23 +3,43 @@
  * @return {number}
  */
 var findCircleNum = function(isConnected) {
-    let pcount = 0, n = isConnected.length, visited = new Array(n).fill(0)
+    let n = isConnected.length
+    let adj = new Array(n).fill().map(() => [])
 
-    for(let i=0; i< n; i++){
-        if(!visited[i]){
-            dfs(i)
-            pcount++
-        }
-    }
-    return pcount 
-
-    function dfs(node){
-        visited[node] = 1
+    for(let i=0; i<n; i++){
         for(let j=0; j<n; j++){
-            if(isConnected[node][j] == 1 && !visited[j]){
-                dfs(j)
+            if(isConnected[i][j] == 1){
+                adj[i].push(j)
+                adj[j].push(i)
             }
         }
     }
+
+    let visited = new Array(n).fill(0)
+    let province = 0
+
+    for(let i=0; i<n; i++){
+        if(!visited[i]){
+            province++
+
+            let q = [i]
+            visited[i] = 1
+
+            while(q.length){
+                for(let i=0; i<q.length; i++){
+                    let node = q.shift()
+                    for(let nbr of adj[node]){
+                        if(!visited[nbr]){
+                            visited[nbr] = 1
+                            q.push(nbr)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return province
+
+
     
 };
