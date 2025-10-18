c9aa4445ac05038420350913e4409295906e82ab
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-18 22:17:25 +0530

    Time: 2 ms (75.00%) | Memory: 56.5 MB (64.98%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.js
+++ 547-number-of-provinces/number-of-provinces.js
@@ -4,42 +4,36 @@
  */
 var findCircleNum = function(isConnected) {
     let n = isConnected.length
-    let adj = new Array(n).fill().map(() => [])
+    // let adj = new Array(n).fill().map(() => [])
 
-    for(let i=0; i<n; i++){
-        for(let j=0; j<n; j++){
-            if(isConnected[i][j] == 1){
-                adj[i].push(j)
-                adj[j].push(i)
-            }
-        }
-    }
+    // for(let i=0; i<n; i++){
+    //     for(let j=0; j<n; j++){
+    //         if(isConnected[i][j] == 1){
+    //             adj[i].push(j)
+    //             adj[j].push(i)
+    //         }
+    //     }
+    // }
 
     let visited = new Array(n).fill(0)
     let province = 0
 
-    for(let i=0; i<n; i++){
-        if(!visited[i]){
-            province++
-
-            let q = [i]
-            visited[i] = 1
-
-            while(q.length){
-                for(let i=0; i<q.length; i++){
-                    let node = q.shift()
-                    for(let nbr of adj[node]){
-                        if(!visited[nbr]){
-                            visited[nbr] = 1
-                            q.push(nbr)
-                        }
-                    }
-                }
+    function dfs(root){
+        visited[root] = 1
+        for(let i=0; i<n; i++){
+            if(isConnected[root][i] == 1 && !visited[i]){
+                dfs(i)
             }
         }
     }
+
+    for(let i=0; i<n; i++){
+        if(!visited[i]){
+            province++
+            dfs(i)
+        }
+    }
+
     return province
-
-
     
 };
