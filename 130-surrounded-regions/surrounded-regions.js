92df93a8d513dab76cce31fdf9852770c3505408
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-24 21:53:10 +0530

    Time: 10 ms (33.48%) | Memory: 60.1 MB (39.31%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.js
+++ 130-surrounded-regions/surrounded-regions.js
@@ -3,13 +3,24 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solve = function(board) {
-    // we need to start from border where 0 are present
-    let m = board.length, n = board[0].length
-    let dir = [[1,0], [0,1], [-1,0], [0,-1]]
 
-    for(let i=0; i<m; i++){
-        for(let j=0; j< n; j++){
-            if(i==0 || j == 0 || i == m-1 || j == n-1){
+    function dfs(x,y){
+        for(let [dx,dy] of dir){
+            let nx = x+dx, ny = y+dy
+            if(nx >= 0 && nx < rows && ny >= 0 && ny < cols && board[nx][ny] == 'O'){
+                board[nx][ny] = '#'
+                dfs(nx,ny)
+            }
+        }
+    }
+
+    let dir = [[0,1], [1,0], [-1,0], [0,-1]]
+
+    let rows = board.length, cols = board[0].length
+
+    for(let i=0; i<rows; i++){
+        for(let j=0; j<cols; j++){
+            if(i==0 || j == 0 || i == rows-1 || j == cols-1){
                 if(board[i][j] == 'O'){
                     board[i][j] = '#'
                     dfs(i,j)
@@ -17,22 +28,13 @@ var solve = function(board) {
             }
         }
     }
-    
-    function dfs(i,j){
-        for(let [di,dj] of dir){
-            let ni = i+di, nj = j+dj
-            if(ni >= 0 && ni < m && nj >= 0 && nj < n && board[ni][nj] == 'O'){
-                board[ni][nj] = '#'
-                dfs(ni,nj)
-            }
-        }
-    }
 
-    for(let i=0; i<m; i++){
-        for(let j=0; j<n; j++){
-            if(board[i][j] == 'O') board[i][j] = 'X'
+    for(let i=0; i<rows; i++){
+        for(let j=0; j<cols; j++){
             if(board[i][j] == '#') board[i][j] = 'O'
-            
+            else{
+                board[i][j] = 'X'
+            }
         }
     }
 };
