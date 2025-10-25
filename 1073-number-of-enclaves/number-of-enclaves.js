3343c55a5b1934bb81c7d1954d130199f6e11b2a
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-25 08:57:31 +0530

    Time: 37 ms (28.87%) | Memory: 70.1 MB (36.62%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.js
+++ 1073-number-of-enclaves/number-of-enclaves.js
@@ -3,38 +3,35 @@
  * @return {number}
  */
 var numEnclaves = function(grid) {
-    let count = 0, m = grid.length, n = grid[0].length
-    let dir = [[1,0], [0,1], [-1,0], [0,-1]]
 
-    function dfs(i,j){
-        for(let [di,dj] of dir){
-            let ni = i+di, nj = j+dj
-            if(ni >= 0 && ni < m && nj >= 0 && nj < n && grid[ni][nj] == 1){
-                grid[ni][nj] = 0
-                dfs(ni,nj)
+    function dfs(x,y){
+        for(let [dx,dy] of dir){
+            let nx = x+dx, ny = y+dy
+            if(nx >= 0 && nx < rows && ny >= 0 && ny < cols && grid[nx][ny] == 1){
+                grid[nx][ny] = '#'
+                dfs(nx,ny)
             }
         }
     }
 
-    for(let i=0; i<m; i++){
-        for(let j=0; j<n; j++){
-            if(i == 0 || i == m-1 || j ==0 || j == n-1){
+    let dir = [[1,0], [0,1], [-1,0], [0,-1]]
+    let rows = grid.length, cols = grid[0].length
+
+    for(let i=0; i<rows; i++){
+        for(let j=0; j<cols; j++){
+            if(i==0 || j == 0 || i == rows-1 || j == cols-1){
                 if(grid[i][j] == 1){
-                    grid[i][j] = 0
+                    grid[i][j] = '#'
                     dfs(i,j)
                 }
             }
         }
     }
-
-    for(let i=0; i<m; i++){
-        for(let j=0; j<n; j++){
+    let count = 0
+    for(let i=0; i<rows; i++){
+        for(let j=0; j<cols; j++){
             if(grid[i][j] == 1) count++
         }
     }
-
     return count
-
-    
-    
 };
