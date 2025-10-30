e6bb10f922ff858fb2ddf6fa737a9f7ed0aceba9
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-30 09:46:40 +0530

    Time: 28 ms (44.44%) | Memory: 65.8 MB (59.72%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.js
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.js
@@ -3,39 +3,38 @@
  * @return {number}
  */
 var sumSubarrayMins = function(arr) {
-    let n = arr.length, stack = [], MOD = 1e9 + 7
-    let left = new Array(n).fill(0)
-    let right = new Array(n).fill(0)
-    ans = 0
+    let s1 = [], s2 = [], n = arr.length
+    let ple = new Array(n).fill(-1)
+    let nle = new Array(n).fill(n)
 
-    // creating previous less elements
-
-    for(let i=0; i<n; i++){
-        let count = 1
-        while (stack.length && arr[i] < stack[stack.length-1][0]) {
-            count += stack.pop()[1];
-        }
-        stack.push([arr[i], count]);
-        left[i] = count
-    }
-
-    stack = []
-
-    for(let i=n-1; i>= 0; i--){
-        let count = 1
-        while(stack.length && arr[i] <= stack[stack.length-1][0]){
-            count += stack.pop()[1]
-        }
-        stack.push([arr[i], count])
-        right[i] = count
-    }
-
-    let result = 0
+    // creating ple
 
     for(let i=0; i<n; i++){
-        result += (arr[i]*left[i]*right[i]) % MOD
+        while(s1.length && arr[s1[s1.length-1]] >= arr[i]){
+            s1.pop()
+        }
+        ple[i] = s1.length ? s1[s1.length-1] : -1
+        s1.push(i)
     }
 
-    return result% MOD
-    
+    // creating nle
+
+    for(let i=0; i<n; i++){
+        while(s2.length && arr[s2[s2.length-1]] >= arr[i]){
+            let idx = s2.pop()
+            nle[idx] = i
+        }
+        s2.push(i)
+    }
+    // finding ans
+    let res = 0, mod = (10**9)+7
+
+    for(let i=0; i<n; i++){
+        let left =  i - ple[i]
+        let right = nle[i] - i
+
+        res = (res + arr[i] * left * right) % mod
+    }
+
+    return res
 };
