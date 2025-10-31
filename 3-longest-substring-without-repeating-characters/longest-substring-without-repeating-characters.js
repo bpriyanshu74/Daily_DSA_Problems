9ecb7f594a358574f62752ab7920fb4a1a99d18b
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-31 11:06:03 +0530

    Time: 121 ms (13.22%) | Memory: 61.8 MB (9.57%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,25 +3,17 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    if(s.length <= 1) return s.length
+    let maxlen = 0
 
-    let maxlen = 1
-
-    let hashmap = {}, l = 0
-
-    for(let r=0; r<s.length; r++){
-        if(hashmap[s[r]] != undefined){
-            let last_index = hashmap[s[r]]
-            if(l > last_index){
-                hashmap[s[r]] = r
-            }else{
-                l = last_index+1
-                hashmap[s[r]] = r
+    for(let i=0; i<s.length; i++){
+        let set = new Set()
+        for(let j=i; j<s.length; j++){
+            if(set.has(s[j])){
+                break
             }
-        }else{
-            hashmap[s[r]] = r
-        }
-        maxlen = Math.max(maxlen, r-l+1)
+            set.add(s[j])
+            maxlen = Math.max(maxlen, j-i+1)
+        }  
     }
     return maxlen
 };
