ffb23900be4af58ea2e1387d33daf57034533dc4
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-31 11:38:19 +0530

    Time: 3 ms (64.70%) | Memory: 60.4 MB (47.73%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.js
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.js
@@ -4,31 +4,15 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-  let prefix = [0]
+    let l=0, minlen = Infinity, sum = 0
 
-  for(let i=0; i<nums.length; i++){
-    prefix.push(nums[i] + prefix[prefix.length-1])
-  }
-
-  let minlen = Infinity
-
-  for(let i=0; i<prefix.length; i++){
-    let targetsum = target + prefix[i]
-
-    let l=0, r= prefix.length-1, j=-1
-
-    while(l <= r){
-        let m = Math.floor((r+l)/2)
-
-        if(prefix[m] >= targetsum){
-            j = m
-            r = m-1
-        }else{
-            l = m+1
+    for(let r=0; r<nums.length; r++){
+        sum += nums[r]
+        while(sum >= target){
+            minlen = Math.min(minlen, r-l+1)
+            sum -= nums[l]
+            l++
         }
     }
-    if (j!= -1) minlen = Math.min(minlen, j-i)
-  }
-
-  return minlen == Infinity ? 0 : minlen
+    return minlen == Infinity ? 0 : minlen
 };
