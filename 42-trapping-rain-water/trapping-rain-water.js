09d1ad77ef8982ccd43cbdbfc618f0ab958046dd
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-31 18:22:58 +0530

    Time: 2 ms (50.70%) | Memory: 57.2 MB (55.24%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.js
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -3,29 +3,26 @@
  * @return {number}
  */
 var trap = function(height) {
-    let lb = new Array(height.length).fill(0)
-    let rb = new Array(height.length).fill(0)
+    let stack = [], water = 0
 
-    // creating the left coundary
-    lb[0] = height[0]
-    for(let i=1; i<height.length; i++){
-        lb[i] = Math.max(lb[i-1], height[i])
+    for(let i=0; i< height.length; i++){
+        while(stack.length && height[i] > height[stack[stack.length-1]]){
+            // let mid = stack.pop();
+            // if (!stack.length) break;
+            // let left = stack[stack.length - 1];
+            // let width = i - left - 1;
+            // let h = Math.min(height[left], height[i]) - height[mid];
+            // water += width * h;
+            let mid = stack.pop()
+            if(!stack.length) break
+
+            let left = stack[stack.length-1]
+
+            let width = i - left - 1
+            let h = Math.min(height[left], height[i]) - height[mid]
+            water += width * h
+        }
+        stack.push(i)
     }
-
-    // creating the right boundary
-    let n = height.length
-    rb[n-1] = height[n-1]
-
-    for(let i = n-2; i>= 0; i--){
-        rb[i] = Math.max(rb[i+1], height[i])
-    }
-
-    // finding the total water
-    let water = 0
-    for(let i=0; i<n; i++){
-        water += Math.min(lb[i], rb[i]) - height[i]
-    }
-
     return water
-    
 };
