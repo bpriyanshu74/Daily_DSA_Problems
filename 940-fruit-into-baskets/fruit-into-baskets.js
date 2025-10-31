938bbf5e56f36c4d1a0d441618a3a3dd6aa8c28a
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-10-31 17:42:22 +0530

    Time: 31 ms (70.12%) | Memory: 67.1 MB (50.44%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.js
+++ 940-fruit-into-baskets/fruit-into-baskets.js
@@ -3,23 +3,24 @@
  * @return {number}
  */
 var totalFruit = function(fruits) {
-    let l=0, r=0, maxcount = 0, map = new Map()
+    // so this is a sliding window problem, to find the maxm window with just 2 fruits
 
-    while( r < fruits.length){
-        map.set(fruits[r], (map.get(fruits[r])||0) +1)
+    let maxcount = 0, l = 0, freq = new Map(), count = 0
 
-        if(map.size > 2){
-            map.set(fruits[l], map.get(fruits[l]) - 1)
-            if(map.get(fruits[l]) == 0){
-                map.delete(fruits[l])
-            }
+    for(let r=0; r<fruits.length; r++){
+        let f = fruits[r]
+        freq.set(f, (freq.get(f) || 0) + 1)
+
+        while(freq.size > 2){
+            maxcount = Math.max(count, maxcount)
+            freq.set(fruits[l], (freq.get(fruits[l])) - 1)
+            count--
+            if(freq.get(fruits[l]) <= 0) freq.delete(fruits[l])
             l++
         }
-        
-        maxcount = Math.max(maxcount, r-l+1)
-        
-        r++
+        count++
     }
-    return maxcount
-    
+
+    return Math.max(count, maxcount)
+
 };
