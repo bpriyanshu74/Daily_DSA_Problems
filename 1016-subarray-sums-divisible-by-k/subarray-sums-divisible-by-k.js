e5f8cbffc2c75a9303fd69e2802d2e402806f49b
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-02 21:51:13 +0530

    Time: 17 ms (61.61%) | Memory: 62.1 MB (17.41%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.js
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.js
@@ -4,15 +4,16 @@
  * @return {number}
  */
 var subarraysDivByK = function(nums, k) {
-    let count = 0, map = {0:1}, prefix = 0
+    let count = 0, freq = {0:1}, prefix = 0
 
     for(let i=0; i<nums.length; i++){
-        prefix = (prefix + nums[i]) % k
+        prefix = (nums[i] + prefix) % k
+
         if(prefix < 0) prefix += k
 
-        count += map[prefix] || 0
+        count += (freq[prefix] || 0)
 
-        map[prefix] = (map[prefix] || 0) + 1
+        freq[prefix] = (freq[prefix] || 0 ) + 1
     }
     return count
     
