fa671a13b98a8c9b9788b83de794b4e1bc22bc77
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-05 17:34:56 +0530

    Time: 64 ms (48.77%) | Memory: 62.6 MB (75.75%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -12,15 +12,13 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    if(!headA || !headB) return null
-
+    if(!headA || !headB) return 0
     let p1 = headA, p2 = headB
 
     while(p1 != p2){
-        p1 = p1 ? p1.next: headB
-        p2 = p2 ? p2.next: headA
+        p1 = p1 ? p1.next : headB
+        p2 = p2 ? p2.next : headA
     }
-
     return p1
-    
+
 };
