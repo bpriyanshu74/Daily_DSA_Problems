97aa2b29826005cda9d02654e248ca482a3ffd01
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-06 12:18:43 +0530

    Time: 48 ms (5.17%) | Memory: 69.2 MB (41.33%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.js
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.js
@@ -4,22 +4,15 @@
  * @return {number}
  */
 var numberOfSubarrays = function(nums, k) {
-    // we'll use 0,1 balancing in this -> marking even as 0 odd as 1
+    let count = 0, map = {0:1}, prefix = 0
 
-    // [1,1,0,1,1]
-
-    let count = 0, prefix = 0, freq = {0:1}
-
-    for(let i=0; i<nums.length; i++){
-        prefix += nums[i] % 2 == 0 ? 0 : 1
-
-        if(prefix-k in freq){
-            count += freq[prefix-k]
+    for(let num of nums){
+        prefix += num % 2 == 0 ? 0 : 1
+        if(prefix-k in map){
+            count += map[prefix-k]
         }
-        freq[prefix] = (freq[prefix] || 0) + 1
+        map[prefix] = (map[prefix] || 0) + 1
     }
 
     return count
-
-
 };
