26b913712c3f207b9e74cfe3dd47aa04769fe99f
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-06 13:34:18 +0530

    Time: 64 ms (41.73%) | Memory: 53.6 MB (74.49%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,19 +3,7 @@
  * @return {number}
  */
 var fib = function(n) {
+    if(n <= 1) return n
 
-    let dp = new Array(n+1).fill(-1)
-    
-    function dfs(n){
-        if(n==0) return 0
-        if(n == 1) return 1
-
-        if(dp[n] != -1) return dp[n]
-
-        dp[n] = dfs(n-1)+dfs(n-2)
-
-        return dp[n]
-    }
-    return dfs(n)
-    
+    return fib(n-1) + fib(n-2)
 };
