02af79e71f11cac8c219ef04746ee79ebe645ce2
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-07 23:15:37 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (63.01%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,17 +4,17 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    if(nums.length == 0) return -1
-    
+    // nums.sort((a,b) => a-b)
     let l = 0, r = nums.length-1
 
-    while(l<=r){
-        let mid = Math.floor((r+l)/2)
+    while(l <= r){
+        let mid = l + Math.floor((r-l)/2)
 
         if(nums[mid] == target) return mid
         else if(nums[mid] > target) r = mid - 1
-        else l = mid+1
+        else{
+            l = mid + 1
+        }
     }
     return -1
-    
 };
