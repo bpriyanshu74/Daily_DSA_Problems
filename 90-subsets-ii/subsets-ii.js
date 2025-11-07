745b7c51641e76ffd7399ed3eb1926d2cfc4e6d7
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-07 11:57:48 +0530

    Time: 1 ms (37.52%) | Memory: 59.3 MB (72.77%) - LeetSync

--- 90-subsets-ii/subsets-ii.js
+++ 90-subsets-ii/subsets-ii.js
@@ -3,18 +3,23 @@
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
-    let res = []
 
-    function dfs(index, path){
-        res.push([...path])
+    let ans = []
+    nums.sort((a,b) => a-b)
+
+    function dfs(path, index){
+        ans.push([...path])
+
         for(let i=index; i<nums.length; i++){
             if(i > index && nums[i] == nums[i-1]) continue
+
             path.push(nums[i])
-            dfs(i+1, path)
+            dfs(path, i+1)
             path.pop()
         }
     }
-    nums.sort((a,b) => a-b)
-    dfs(0, [])
-    return res
+
+    dfs([], 0)
+    return ans
+    
 };
