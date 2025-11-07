017fa0260e1a9805e672463ca59a8f8c97ba6e3b
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-07 23:35:36 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (55.27%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,33 +4,37 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
+    let pos = [-1, -1]
 
-    function lowerBound() {
-        let l = 0, r = nums.length;
-        while (l < r) {
-        let mid = Math.floor((l + r) / 2);
-        if (nums[mid] < target) l = mid + 1;
-        else r = mid;
+    function lowerbound(){
+        let l=0, r=nums.length
+
+        while(l < r){
+            let mid = l + Math.floor((r-l)/2)
+
+            if(nums[mid] < target) l = mid + 1
+            else r = mid
         }
-        return l;
-    }
-    function upperBound() {
-        let l = 0, r = nums.length;
-        while (l < r) {
-        let mid = Math.floor((l + r) / 2);
-        if (nums[mid] <= target) l = mid + 1;
-        else r = mid;
-        }
-        return l-1;
+        return l
     }
 
-    let res = [-1, -1]
-    let first = lowerBound()
-    let last = upperBound()
+    function upperbound(){
+        let l=0, r = nums.length
 
-    if(nums[first] == target) res[0] = first
-    if(nums[last] == target) res[1] = last
+        while(l < r){
+            let mid = l + Math.floor((r-l)/2)
 
-    return res
-    
+            if(nums[mid] <= target) l = mid + 1
+            else r = mid
+        }
+        return l
+    }
+
+    let lb = lowerbound()
+    let ub = upperbound() - 1
+
+    if(nums[lb] == target) pos[0] = lb
+    if(nums[ub] == target) pos[1] = ub
+
+    return pos
 };
