e6f09492d52cfe195a544cc1e924ce6cb1bf0c3a
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-07 23:41:26 +0530

    Time: 1 ms (67.21%) | Memory: 55.4 MB (76.06%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    let l = 1, r = Math.floor(x/2)+1
+    if(x <= 1) return x
+    let l=1, r = x-1
 
     while(l <= r){
         let mid = l + Math.floor((r-l)/2)
-        let sqrt = mid*mid
 
-        if(sqrt == x) return mid
-        else if(sqrt < x) l = mid + 1
+        if(mid*mid == x) return mid
+        else if(mid*mid < x) l = mid + 1
         else r = mid - 1
     }
     return r
-    
 };
