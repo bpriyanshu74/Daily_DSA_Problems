4e3c106c7670a3a8a01fcfb9253394cc292d74f8
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-08 21:18:09 +0530

    Time: 3 ms (67.30%) | Memory: 58 MB (35.60%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,22 +4,21 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    let hashmap = new Map(), stack = []
+    let stack = [], map = {}, ans = []
 
     for(let num of nums2){
         while(stack.length && num > stack[stack.length-1]){
-            hashmap.set(stack.pop(), num)
+            map[stack.pop()] = num
         }
         stack.push(num)
     }
 
     while(stack.length){
-        hashmap.set(stack.pop(), -1)
+        map[stack.pop()] = -1
     }
 
     for(let i=0; i<nums1.length; i++){
-        nums1[i] = hashmap.get(nums1[i])
+        ans.push(map[nums1[i]])
     }
-
-    return nums1
+    return ans
 };
