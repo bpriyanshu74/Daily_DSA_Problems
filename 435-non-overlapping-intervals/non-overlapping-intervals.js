aa8d285644f09880632f4cc0eb549470f7685881
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-09 22:30:01 +0530

    Time: 75 ms (93.45%) | Memory: 84.8 MB (93.76%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.js
+++ 435-non-overlapping-intervals/non-overlapping-intervals.js
@@ -3,20 +3,20 @@
  * @return {number}
  */
 var eraseOverlapIntervals = function(intervals) {
-    let count = 0
-    if(intervals.length <= 1) return count
+    if(intervals.length == 1) return 0
 
+    // sorted intervals by end time
     intervals.sort((a,b) => a[1] - b[1])
-
-    let [start, end] = intervals[0]
-
+    //now we'll count the interval that overlaps and return it
+    let count = 0, [start, end] = intervals[0]
     for(let i=1; i<intervals.length; i++){
-        let [cur_start, cur_end] = intervals[i]
-        if(end <= cur_start){
-            end = cur_end
-        }else{
-            count++
+        let [s,e] = intervals[i]
+        if(s < end || s < start) count++
+        else{
+            start = s
+            end = e
         }
     }
+
     return count
 };
