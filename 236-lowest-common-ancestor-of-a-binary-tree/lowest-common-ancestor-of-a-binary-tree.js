861b5349c540ff9e04574dfe89f29a89ab6dd6ff
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-09 13:32:58 +0530

    Time: 49 ms (96.08%) | Memory: 65.9 MB (47.30%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.js
@@ -14,10 +14,10 @@
 var lowestCommonAncestor = function(root, p, q) {
     if(root == null || root == p || root == q) return root
 
-    let left = lowestCommonAncestor(root.left,p, q)
+    let left = lowestCommonAncestor(root.left, p, q)
     let right = lowestCommonAncestor(root.right, p, q)
 
     if(left && right) return root
-
-    return left || right
+    if(left) return left
+    if(right) return right
 };
