91c63a1adf07b1922d4ab3f4cfef02614d877e83
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-11 23:15:08 +0530

    Time: 0 ms (100.00%) | Memory: 55.6 MB (20.90%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -3,26 +3,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    let low = 0, high = nums.length-1, mid = 0
-
-    function swap(i,j){
-        [nums[i], nums[j]] = [nums[j], nums[i]]
-    }
+    let low = 0, mid = 0, high = nums.length-1
 
     while(mid <= high){
         if(nums[mid] == 0){
-            swap(mid,low)
+            [nums[mid], nums[low]] = [nums[low], nums[mid]]
             mid++
             low++
-        }
-        else if(nums[mid] == 1){
-            mid++
-        }
-        else{
-            swap(mid, high)
+        }else if(nums[mid] == 2){
+            [nums[mid], nums[high]] = [nums[high], nums[mid]]
             high--
+        }else{
+            mid++
         }
     }
     return nums
-    
 };
