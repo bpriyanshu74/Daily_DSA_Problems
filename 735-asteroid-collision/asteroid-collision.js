fd18443c26f04a2112f15b61e169656fc5aa9038
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-12 12:40:30 +0530

    Time: 5 ms (52.24%) | Memory: 56.3 MB (81.38%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.js
+++ 735-asteroid-collision/asteroid-collision.js
@@ -7,7 +7,7 @@ var asteroidCollision = function(asteroids) {
 
     for(let ast of asteroids){
         let destroyed = false
-        while(stack.length && ast < 0 && stack[stack.length-1] > 0){
+        while(stack.length && (stack[stack.length-1] > 0 && ast < 0)){
             if(Math.abs(ast) > stack[stack.length-1]){
                 stack.pop()
                 continue
@@ -20,11 +20,11 @@ var asteroidCollision = function(asteroids) {
                 break
             }
         }
-
         if(!destroyed){
             stack.push(ast)
         }
     }
+
     return stack
-    
+    z
 };
