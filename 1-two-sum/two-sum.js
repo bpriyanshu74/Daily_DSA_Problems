d7e4f56ab73aac7fe887d906764230c59779618d
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-12 16:31:27 +0530

    Time: 3 ms (61.08%) | Memory: 56.9 MB (41.74%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,15 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = {}
+    
+    let map = new Map()
 
     for(let i=0; i<nums.length; i++){
-        let diff = target - nums[i]
-        if(diff in map){
-            return [i, map[diff]]
+        let diff = target-nums[i]
+
+        if(map.has(diff)){
+            return [i, map.get(diff)]
         }
-        map[nums[i]] = i
+
+        map.set(nums[i], i)
     }
 
-    
+    return [-1,-1]
 };
