c23073096cbcef1796d86fa24363822c316fe34c
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-12 19:26:02 +0530

    Time: 1 ms (93.94%) | Memory: 64.2 MB (13.54%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -3,20 +3,21 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let l = 0, r = height.length-1, maxwater = 0
+    let l=0, r = height.length-1, maxwater = 0
 
     while(l < r){
         let h = Math.min(height[l], height[r])
+
         let w = r-l
 
         maxwater = Math.max(maxwater, h*w)
 
-        if(height[l] <= height[r]){
+        if(height[l] < height[r]){
             l++
         }else{
             r--
         }
     }
-
     return maxwater
+    
 };
