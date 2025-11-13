2c9e0a04a927dccbb7eb50a709028332c00d5d16
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-13 12:21:41 +0530

    Time: 0 ms (100.00%) | Memory: 56.1 MB (97.44%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -5,10 +5,11 @@
 var removeDuplicates = function(nums) {
     let k = 0
     for(let i=1; i<nums.length; i++){
-        if(nums[k] != nums[i]){
-            nums[++k] = nums[i]
+        if(nums[i] == nums[k]) continue
+        else{
+            k++
+            nums[k] = nums[i]
         }
     }
     return k+1
-    
 };
