3c3cd789d5c64a086e14eb32d581a082e7075474
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-13 13:56:38 +0530

    Time: 0 ms (100.00%) | Memory: 55 MB (77.31%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -6,20 +6,23 @@
 var searchRange = function(nums, target) {
     let pos = [-1, -1]
 
-    function lowerbound(){
-        let l=0, r=nums.length
+    function lowerBound(){
+        let l = 0, r = nums.length
 
         while(l < r){
-            let mid = l + Math.floor((r-l)/2)
+            let mid = l + Math.floor((r-l) / 2)
 
-            if(nums[mid] < target) l = mid + 1
-            else r = mid
+            if(nums[mid] < target){
+                l = mid + 1
+            }else{
+                r = mid
+            }
         }
         return l
     }
 
-    function upperbound(){
-        let l=0, r = nums.length
+    function upperBound(){
+        let l = 0, r = nums.length
 
         while(l < r){
             let mid = l + Math.floor((r-l)/2)
@@ -30,11 +33,12 @@ var searchRange = function(nums, target) {
         return l
     }
 
-    let lb = lowerbound()
-    let ub = upperbound() - 1
+    let lb = lowerBound()
+    let ub = upperBound() - 1
 
-    if(nums[lb] == target) pos[0] = lb
-    if(nums[ub] == target) pos[1] = ub
+    if(lb < nums.length && nums[lb] == target) pos[0] = lb
+    if(ub < nums.length && nums[ub] == target) pos[1] = ub
 
     return pos
+    
 };
