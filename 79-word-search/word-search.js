a72ef88f976b5cd7201d5fc722e2b2deae906336
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-14 22:34:49 +0530

    Time: 309 ms (60.95%) | Memory: 54.6 MB (63.10%) - LeetSync

--- 79-word-search/word-search.js
+++ 79-word-search/word-search.js
@@ -5,30 +5,33 @@
  */
 var exist = function(board, word) {
 
-    function dfs(x,y,index){
+    function dfs(s,e, index){
         if(index == word.length) return true
+        
+        if( s < 0 || s >= board.length || e < 0 || e >= board[0].length || board[s][e] != word[index]) return false
+        
+        let temp = board[s][e]
 
-        if(x < 0 || x >= board.length || y < 0 || y >= board[0].length || board[x][y] != word[index]) return 
+        board[s][e] = '#'
 
-        let temp = board[x][y]
-        board[x][y] = '#'
+        let found = dfs(s+1, e, index+1) ||
+                    dfs(s-1, e, index+1) ||
+                    dfs(s, e+1, index+1) ||
+                    dfs(s, e-1, index+1)
 
-        let found = (dfs(x+1,y,index+1) || 
-                        dfs(x-1, y, index+1) ||
-                        dfs(x, y+1, index+1) ||
-                        dfs(x, y-1, index+1))
-        board[x][y] = temp
+        board[s][e] = temp
+
         return found
     }
-
-
-
+    
     for(let i=0; i<board.length; i++){
         for(let j=0; j<board[0].length; j++){
             if(board[i][j] == word[0]){
-                if(dfs(i,j,0)) return true
+                if(dfs(i,j, 0)) return true
             }
         }
-    }    
+    }
+
     return false
+    
 };
