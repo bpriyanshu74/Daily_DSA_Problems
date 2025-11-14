b79216d39078f0d7ca93ee655edb644cc39ae8dc
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-14 21:05:34 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (80.00%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -3,14 +3,14 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    let dp = new Array(n+1).fill(-1)
-    function dfs(n){
-        if(n <= 1) return 1
-        if(dp[n] != -1) return dp[n]
+    let dp = new Array(n+1).fill(0)
 
-        return dp[n] = dfs(n-1)+dfs(n-2)
+    dp[1] = 1
+    dp[2] = 2
+
+    for(let i=3 ; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2]
     }
 
-    return dfs(n)
-    
+    return dp[n]
 };
