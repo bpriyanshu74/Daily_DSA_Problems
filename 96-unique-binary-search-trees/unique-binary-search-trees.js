8f8349491804d16c57986e6abf7d358004a26250
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-15 22:03:45 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (8.00%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+    let dp = new Array(n+1).fill(0)
+
+    dp[0] = 1
+    dp[1] = 1
+
+    for(let nodes = 2; nodes <=n; nodes++){
+        for(let root = 1; root <= nodes; root++){
+            dp[nodes] += dp[root-1] * dp[nodes-root]
+        }
+    }
+
+    return dp[n]
+};
