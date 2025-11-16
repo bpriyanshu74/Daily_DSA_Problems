7c10537dacc4ae3c140dafc02d060d97ed79e22c
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-16 12:40:46 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (24.76%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/pascals-triangle-ii.js
@@ -3,28 +3,12 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
+    let row = [1], n = rowIndex
 
-    function nCr(n, r) {
-        let res = 1;
-    
-        // calculating nCr:
-        for (let i = 0; i < r; i++) {
-        res = res * (n - i);
-        res = res / (i + 1);
-        }
-        return res;
+    for(let i=1 ; i<=n; i++){
+        let prev = row[i-1]
+        row.push(prev * (n-i+1)/i)
     }
 
-    let n = rowIndex+1
-    
-    // printing the entire row n:
-    let temp = []
-    for (let c = 1; c <= n; c++) {
-        temp.push(nCr(n-1,c-1))
-    }
-    return temp
-    
-  
-
-    
+    return row
 };
