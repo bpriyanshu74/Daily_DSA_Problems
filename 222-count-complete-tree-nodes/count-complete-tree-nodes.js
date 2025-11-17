b87bec8d8ac4b4d56fc89ebc3523b522491bfb48
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-17 16:58:51 +0530

    Time: 31 ms (5.57%) | Memory: 75.6 MB (6.04%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.js
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.js
@@ -11,31 +11,17 @@
  * @return {number}
  */
 var countNodes = function(root) {
+    if(!root) return 0
 
-    if(root == null) return 0
+    let q = [root], count = 0
 
-    let lh = findh(root,'l')
-    let rh = findh(root, 'r')
+    while(q.length){
+        count++
+        let node = q.shift()
 
-    if(lh == rh) return (1<<lh)-1
-    return 1 + countNodes(root.left) + countNodes(root.right)
-
-    function findh(node, dir){
-        if(dir == 'l'){
-            let h = 0
-            while(node){
-                h++
-                node = node.left
-            }
-            return h
-        }
-        if(dir == 'r'){
-            let h = 0
-            while(node){
-                h++
-                node = node.right
-            }
-            return h
-        }
+        if(node.left) q.push(node.left)
+        if(node.right) q.push(node.right)
     }
+
+    return count
 };
