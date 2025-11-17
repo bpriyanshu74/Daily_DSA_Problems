508a21b21113cab773e53a8a221cb6bd2112a2cf
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-17 17:29:45 +0530

    Time: 0 ms (100.00%) | Memory: 54.7 MB (98.47%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -3,5 +3,11 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    return n>0 && (n &(n-1)) === 0
+    
+    while(n > 1){
+        if(n % 2 != 0) return false
+        n = n/2
+    }
+
+    return n == 1
 };
