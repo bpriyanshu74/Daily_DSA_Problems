d5aedce4e51b7776425da1d2e7ce7799a649db16
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-20 12:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (99.24%) - LeetSync

--- 198-house-robber/house-robber.js
+++ 198-house-robber/house-robber.js
@@ -5,21 +5,36 @@
 var rob = function(nums) {
     let n = nums.length
 
-    let dp = new Array(n).fill(-1)
+    let dp = new Array(n).fill(0)
+    dp[0] = nums[0]
 
-    function dfs(index){
-        if(index == 0) return nums[index]
-        if(index < 1) return 0
+    for(let i=1; i<n; i++){
+        let take = nums[i] + (i > 1 ? dp[i-2] : 0)
+        let nontake = dp[i-1]
 
-        if(dp[index] != -1) return dp[index]
-
-        let pick = nums[index] + dfs(index-2)
-        let notpick = dfs(index-1)
-
-        dp[index] = Math.max(pick, notpick)
-
-        return dp[index]
+        dp[i] = Math.max(take,nontake)
     }
 
-    return dfs(n-1)
+    return dp[n-1]
+
+    // memoization
+    // function dfs(index){
+    //     if(index == 0) return nums[index]
+    //     if(index < 1) return 0
+
+    //     if(dp[index] != -1) return dp[index]
+
+    //     let pick = nums[index] + dfs(index-2)
+    //     let notpick = dfs(index-1)
+
+    //     dp[index] = Math.max(pick, notpick)
+
+    //     return dp[index]
+    // }
+
+    // return dfs(n-1)
+
+
+    // tabulation
+
 };
