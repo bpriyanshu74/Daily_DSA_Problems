1c5f10890b1882d8874983545bf963d162042935
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-23 01:33:19 +0530

    Time: 18 ms (63.43%) | Memory: 65.4 MB (28.45%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.js
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -4,18 +4,21 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    let map = new Map(), prefix = 0, count = 0
+    let count = 0
+
+    let map = new Map(), prefix = 0
     map.set(0, 1)
 
-    for(let i=0; i<nums.length; i++){
-        prefix += nums[i]
+    for(let num of nums){
+        prefix += num
 
-        if(map.has(prefix-k)){
-            count += map.get(prefix-k)
+        if(map.has(prefix - k)){
+            count += map.get(prefix - k)
         }
 
         map.set(prefix, (map.get(prefix) || 0) + 1)
     }
 
     return count
+    
 };
