60b26fb385337b13d4901becf66d96c44db67419
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-23 15:56:49 +0530

    Time: 88 ms (51.57%) | Memory: 89.5 MB (30.19%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.js
+++ 518-coin-change-ii/coin-change-ii.js
@@ -5,22 +5,24 @@
  */
 var change = function(amount, coins) {
     let n = coins.length
-    let dp = Array.from({length: n}, () => new Array(amount+1).fill(-1))
+    let dp = Array.from({length: n}, () => new Array(amount+1).fill(0))
 
-    function recur(index, amt){
-        if(amt == 0) return 1
-        if(index == n || amt < 0) return 0
-
-        if(dp[index][amt] != -1) return dp[index][amt]
-
-        let take = recur(index, amt - coins[index])
-        let nottake = recur(index+1, amt)
-
-        return dp[index][amt] = take + nottake
-
+    for(let amt = 0; amt <= amount; amt++){
+        dp[0][amt] = amt % coins[0] == 0 ? 1 : 0
     }
 
-    return recur(0, amount)
+    for(let index = 1; index < n; index++){
+        for(let amt = 0; amt <= amount; amt++){
+            let nottake = dp[index-1][amt]
+            let take = 0
+            if(coins[index] <= amt){
+                take = dp[index][amt - coins[index]]
+            }
+            dp[index][amt] = take + nottake
+        }
+    }
+
+    return dp[n-1][amount]
     
     
 };
