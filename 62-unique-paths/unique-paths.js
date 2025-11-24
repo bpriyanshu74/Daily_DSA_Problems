825af7a7bef6fb07f2c382e398ca87bd5fbd735a
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-24 22:58:17 +0530

    Time: 1 ms (40.16%) | Memory: 54 MB (66.02%) - LeetSync

--- 62-unique-paths/unique-paths.js
+++ 62-unique-paths/unique-paths.js
@@ -4,40 +4,18 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
+    let dp = Array.from({length: m}, () => new Array(n).fill(-1))
+    function f(i,j){
+        if(i==0 && j == 0) return 1
+        if(i < 0 || j < 0) return 0
 
-    // recursion method -- count all the paths
-
-    // function total_paths(i,j,m,n){
-    //     if(i==m-1 && j== n-1) return 1
-    //     if(i >= m || j >= n) return 0
-
-    //     let right = total_paths(i,j+1,m,n)
-    //     let down = total_paths(i+1,j,m,n)
-
-    //     return right+down
-    // }
-
-    // return total_paths(0,0,m,n)
-
-    // time complexity is exponential 2^(mxn)
-
-    // we can reduce it by using dp with memoization
-
-    let dp = new Array(m).fill(null).map(() => new Array(n).fill(-1))
-
-    function dynamic_recur(i,j,m,n,dp){
-        if(i == m-1 && j == n-1)return 1
-        if(i >= m || j >= n) return 0
         if(dp[i][j] != -1) return dp[i][j]
 
-        let right = dynamic_recur(i,j+1,m,n,dp)
-        let down = dynamic_recur(i+1,j,m,n,dp)
-        
-        dp[i][j] = right + down
+        let left = f(i, j-1)
+        let right = f(i-1, j)
 
-        return dp[i][j]
+        return dp[i][j] = left + right
     }
 
-    return dynamic_recur(0,0,m,n,dp)
-    
+    return f(m-1, n-1)
 };
