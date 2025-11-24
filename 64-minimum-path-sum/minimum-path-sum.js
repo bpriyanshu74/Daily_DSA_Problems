ac90e5e6445ba1bfc3f3ba18d6e1c0f71dd8aa76
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-24 23:42:19 +0530

    Time: 5 ms (72.43%) | Memory: 56.7 MB (68.02%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.js
+++ 64-minimum-path-sum/minimum-path-sum.js
@@ -4,20 +4,22 @@
  */
 var minPathSum = function(grid) {
     let m = grid.length, n = grid[0].length
-    let dp = Array.from({length: m}, () => new Array(n).fill(0))
+    let dp = Array.from({length: m}, () => new Array(n).fill(-1))
 
-    dp[0][0] = grid[0][0]
-    for(let i=0; i<m; i++){
-        for(let j=0; j<n; j++){
-            if(i == 0 && j == 0) continue
-
-            let left = j > 0 ? dp[i][j-1] : Infinity
-            let up = i > 0 ? dp[i-1][j] : Infinity
-
-            dp[i][j] = grid[i][j] + Math.min(left, up)
+    function f(i,j){
+        if(i == 0 && j == 0){
+            return grid[0][0]
         }
+        if(i < 0 || j < 0) return Infinity
+
+        if(dp[i][j] != -1) return dp[i][j]
+
+        let left = f(i, j-1)
+        let up = f(i-1, j)
+
+        return dp[i][j] =  grid[i][j] + Math.min(left, up)
     }
 
-    return dp[m-1][n-1]
+    return f(m-1, n-1)
     
 };
