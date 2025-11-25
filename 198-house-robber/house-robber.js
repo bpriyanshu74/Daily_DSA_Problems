2cfa7da0ff9d3bb6e44865a684aa292e1c843763
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-11-25 10:17:06 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (53.28%) - LeetSync

--- 198-house-robber/house-robber.js
+++ 198-house-robber/house-robber.js
@@ -3,53 +3,20 @@
  * @return {number}
  */
 var rob = function(nums) {
-    let n = nums.length
+    let dp = new Array(nums.length).fill(-1)
 
-    let prev2 = 0
-    let prev = nums[0]
+    function f(index){
+        if(index == 0) return nums[0]
+        if(index < 0) return 0
+        if(dp[index] != -1) return dp[index]
 
-    for(let i=1; i<n; i++){
-        let take = nums[i] + prev2
-        let nontake = prev
+        let nottake = f(index-1)
+        take = nums[index] + f(index-2)
 
-        let curi = Math.max(take, nontake)
-
-        prev2 = prev
-        prev = curi
+        return dp[index] = Math.max(take, nottake)
+        
     }
 
-    return prev
-
-    // let dp = new Array(n).fill(0)
-    // dp[0] = nums[0]
-
-    // for(let i=1; i<n; i++){
-    //     let take = nums[i] + (i > 1 ? dp[i-2] : 0)
-    //     let nontake = dp[i-1]
-
-    //     dp[i] = Math.max(take,nontake)
-    // }
-
-    // return dp[n-1]
-
-    // memoization
-    // function dfs(index){
-    //     if(index == 0) return nums[index]
-    //     if(index < 1) return 0
-
-    //     if(dp[index] != -1) return dp[index]
-
-    //     let pick = nums[index] + dfs(index-2)
-    //     let notpick = dfs(index-1)
-
-    //     dp[index] = Math.max(pick, notpick)
-
-    //     return dp[index]
-    // }
-
-    // return dfs(n-1)
-
-
-    // tabulation
-
+    return f(nums.length-1)
+    
 };
