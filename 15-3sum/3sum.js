0fe026d954c42c6d575446521521cadf765d505e
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-12-08 12:12:59 +0530

    Time: 37 ms (56.55%) | Memory: 71.8 MB (81.16%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,28 +3,31 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let ans = []
+    let triplets = []
     nums.sort((a,b) => a-b)
+
     for(let i=0; i<nums.length-2; i++){
         if(i > 0 && nums[i] == nums[i-1]) continue
-        let l = i+1, r = nums.length-1
 
-        while(l < r){
-            let sum = nums[i] + nums[l] + nums[r]
+        let p1 = i+1, p2 = nums.length-1
+
+        while(p1 < p2){
+            let sum = nums[i] + nums[p1] + nums[p2]
+
             if(sum == 0){
-                ans.push([nums[i], nums[l], nums[r]])
-                while(l < r && nums[l] == nums[l+1])l++
-                while(l < r && nums[r] == nums[r-1]) r--
-                l++
-                r--
-            }else if(sum < 0){
-                l++
+                triplets.push([nums[i], nums[p1], nums[p2]])
+                p1++
+                p2--
+
+                while(p1 < p2 && nums[p1] == nums[p1-1]) p1++
+                while(p1 < p2 && nums[p2] == nums[p2+1]) p2-- 
+            }
+            else if(sum < 0){
+                p1++
             }else{
-                r--
+                p2--
             }
         }
     }
-
-    return ans
-    
+    return triplets
 };
