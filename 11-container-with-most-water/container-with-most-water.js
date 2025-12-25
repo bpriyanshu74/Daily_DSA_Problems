b067677524d605eb724ab5745101a8631fa4e6da
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-12-25 21:11:53 +0530

    Time: 4 ms (16.67%) | Memory: 63 MB (68.26%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -1,32 +1,22 @@
 /**
  * @param {number[]} height
- * @return {number}
+ * @return {number} 
  */
 var maxArea = function(height) {
-    // brute force
-
-    let maxm = -Infinity
-    // for(let i=0; i<height.length-1; i++){
-    //     for(j=i+1; j<height.length; j++){
-    //         let area = Math.min(height[i], height[j]) * (j-i)
-    //         maxm = Math.max(maxm, area)
-    //     }
-    // }
-
-
-    // optimising it using two pointers
-
-    let l = 0, r= height.length-1
+    let l = 0, r = height.length-1, maxwater = 0
 
     while(l < r){
-        let area = Math.min(height[l], height[r]) * (r-l)
-        maxm = Math.max(maxm, area)
+        let h = Math.min(height[l], height[r])
+        let b = r-l
 
-        if(height[l] <= height[r]){
+        maxwater = Math.max(maxwater, h*b)
+
+        if(height[l] < height[r]){
             l++
         }else{
             r--
         }
     }
-    return maxm
+
+    return maxwater
 };
