55ee20e609b2be1f833f9eb1fd89b2870edaaecd
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2025-12-25 21:38:54 +0530

    Time: 2 ms (88.68%) | Memory: 57.8 MB (16.98%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.js
+++ 985-bag-of-tokens/bag-of-tokens.js
@@ -4,26 +4,31 @@
  * @return {number}
  */
 var bagOfTokensScore = function(tokens, power) {
-    let left = 0, right = tokens.length-1
-    let score = 0
+    // score = 0, tokens = [...], power = p
+
+    // face-up = reduce power gain +1 score
+    // face-down = reduce score by 1 to get that token power
+
     tokens.sort((a,b) => a-b)
-    while(left <= right){
-        if(tokens[left] <= power){
-            power -= tokens[left]
-            left++
-            score++
+
+    let l = 0, r = tokens.length-1, score = 0, maxscore = 0
+
+    // edge case 
+    if(power < tokens[0]) return 0
+
+    while(l <= r){
+        // intially our score is 0, we can only play face-up
+        if(score == 0 && power < tokens[l]) break
+
+        if(power >= tokens[l]){
+            score += 1
+            power -= tokens[l++]
+        }else{
+            score -= 1
+            power += tokens[r--]
         }
-        else{
-            if(score == 0) return 0
-            else{
-                power += tokens[right]
-                right-- 
-                if(right >= left) score --
-            }
-        }
-        
+        maxscore = Math.max(maxscore, score)
     }
-    console.log(power)
-    return score
-    
+
+    return maxscore
 };
