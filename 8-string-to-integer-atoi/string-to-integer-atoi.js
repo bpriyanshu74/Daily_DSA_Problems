3882b815a7eaa251d4369576cdb7e5a77949af35
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-02-07 16:38:02 +0530

    Time: 1 ms (88.15%) | Memory: 57 MB (66.13%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/string-to-integer-atoi.js
@@ -3,24 +3,35 @@
  * @return {number}
  */
 var myAtoi = function(s) {
-    let i=0, sign = 1, result = 0
-    let max = (2**31) - 1, min = -(2**31)
 
-    while(i < s.length && s[i] == ' ') i++
+    function helper(i, sign, result){
+        if(i >= s.length || s[i] < '0' || s[i] > '9'){
+            return sign * result
+        }
+
+        let digit = s[i] - '0'
+
+        if (
+            sign * (result * 10 + digit) > max ||
+            sign * (result * 10 + digit) < min
+        ) {
+            return sign === 1 ? max : min;
+        }
+
+        return helper(i+1, sign, result*10+digit)
+    }
+
+    let i=0, sign = 1
+    let max = (2**31) - 1
+    let min = -(2**31)
+
+    while(i<s.length && s[i] == ' ') i++
 
     if(s[i] == '+' || s[i] == '-'){
         if(s[i] == '-') sign = -1
         i++
     }
 
-    while(i < s.length && s[i] >= '0' && s[i] <= '9'){
-        result = result * 10 + (s[i] - '0')
-
-        if(sign == 1 && result > max) return max
-        if(sign == -1 && -result < min) return min
-        i++
-    }
-
-    return result * sign
+    return helper(i,sign,0)
+    
 };
-
