50ff1071e3245069d0dc8888d3322dbcc55d650a
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-02-07 12:57:27 +0530

    Time: 0 ms (100.00%) | Memory: 57 MB (67.99%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/string-to-integer-atoi.js
@@ -3,26 +3,24 @@
  * @return {number}
  */
 var myAtoi = function(s) {
-    s = s.trim()
-    if(s.length == 0) return 0
+    let i=0, sign = 1, result = 0
+    let max = (2**31) - 1, min = -(2**31)
 
-    let i = 0, sign = 1, result = 0
+    while(i < s.length && s[i] == ' ') i++
 
-    if(s[i] == '-' || s[i] == '+'){
+    if(s[i] == '+' || s[i] == '-'){
         if(s[i] == '-') sign = -1
         i++
     }
 
     while(i < s.length && s[i] >= '0' && s[i] <= '9'){
-        let digit = s[i].charCodeAt(0) - '0'.charCodeAt(0)
+        result = result * 10 + (s[i] - '0')
 
-        if(result > Math.floor((2**31-1-digit)/10)){
-            return sign == 1 ? (2**31) -1 : -(2**31)
-        }
-
-        result = result * 10 + digit
+        if(sign == 1 && result > max) return max
+        if(sign == -1 && -result < min) return min
         i++
     }
 
     return result * sign
 };
+
