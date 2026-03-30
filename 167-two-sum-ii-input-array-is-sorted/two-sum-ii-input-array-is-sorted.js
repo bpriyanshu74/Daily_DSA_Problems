d079be6383c1348f20a83e3f9a8fc3c67a748186
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-03-30 10:07:01 +0530

    Time: 2 ms (62.91%) | Memory: 56.7 MB (37.75%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -4,21 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let res = []
-
     let l = 0, r = numbers.length-1
 
     while(l < r){
         let sum = numbers[l] + numbers[r]
-        if(sum == target){
-            res.push(l+1)
-            res.push(r+1)
-            break
-        }else if(sum > target){
-            r--
-        }else{
-            l++
-        }
+        if(sum == target) return [l+1, r+1]
+        else if(sum < target) l++
+        else r--
     }
-    return res
 };
