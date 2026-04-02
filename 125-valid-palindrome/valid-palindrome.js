e7a351706461b797abfb5c46a895aece8606fae4
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-04-03 00:47:07 +0530

    Time: 6 ms (51.11%) | Memory: 58.1 MB (52.39%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,24 +3,20 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    function isAlphaNum(ch){
-        return (
-            (ch >= 'a' && ch <= 'z') ||
-            (ch >= 'A' && ch <= 'Z') ||
-            (ch >= '0' && ch <= '9')
-        );
+    function isAlpha(ch){
+        return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')
     }
 
     let l = 0, r = s.length-1
 
     while(l < r){
-        while(l < r && !isAlphaNum(s[l])) l++
-        while(l < r && !isAlphaNum(s[r])) r--
+        while(l < r && !isAlpha(s[l])) l++
+        while(l < r && !isAlpha(s[r])) r--
 
         if(s[l].toLowerCase() != s[r].toLowerCase()) return false
         l++
         r--
     }
+
     return true
-    
 };
