53b508e9491f455f0e7d36d3280dd23300f8af5b
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-04-03 01:01:06 +0530

    Time: 30 ms (90.31%) | Memory: 70.6 MB (97.31%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,27 +3,29 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let ans = []
+    let res = []
     nums.sort((a,b) => a-b)
 
     for(let i=0; i<nums.length-2; i++){
         if(nums[i] > 0) break
         if(i > 0 && nums[i] == nums[i-1]) continue
-        let l=i+1, r = nums.length-1
+
+        let l = i+1, r = nums.length-1
 
         while(l < r){
-            
             let sum = nums[i] + nums[l] + nums[r]
 
             if(sum == 0){
-                ans.push([nums[i], nums[l], nums[r]])
+                res.push([nums[i], nums[l], nums[r]])
                 l++
                 r--
                 while(l < r && nums[l] == nums[l-1]) l++
                 while(l < r && nums[r] == nums[r+1]) r--
-            }else if(sum > 0) r--
-            else l++
+            }else if(sum < 0){
+                l++
+            }else r--
         }
     }
-    return ans
+
+    return res
 };
