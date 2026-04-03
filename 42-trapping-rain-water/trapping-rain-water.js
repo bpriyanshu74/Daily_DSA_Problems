2d4d545e6d4499a8574b18fa0659dc8313d8b1a2
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-04-03 12:10:03 +0530

    Time: 12 ms (26.11%) | Memory: 59.2 MB (36.57%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.js
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -2,27 +2,30 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(height) {
-    let stack = [], water = 0
+var trap = function(height){
+    let lb = new Array(height.length).fill(0)
+    let rb = new Array(height.length).fill(0)
+    let trap = 0
 
-    for(let i=0; i< height.length; i++){
-        while(stack.length && height[i] > height[stack[stack.length-1]]){
-            // let mid = stack.pop();
-            // if (!stack.length) break;
-            // let left = stack[stack.length - 1];
-            // let width = i - left - 1;
-            // let h = Math.min(height[left], height[i]) - height[mid];
-            // water += width * h;
-            let mid = stack.pop()
-            if(!stack.length) break
-
-            let left = stack[stack.length-1]
-
-            let width = i - left - 1
-            let h = Math.min(height[left], height[i]) - height[mid]
-            water += width * h
+    for(let i=0; i<height.length; i++){
+        if(i == 0) lb[i] = height[i]
+        else{
+            lb[i] = Math.max(lb[i-1], height[i])
         }
-        stack.push(i)
     }
-    return water
+
+    for(let i=height.length-1; i>=0; i--){
+        if(i == height.length-1){
+            rb[i] = height[i]
+        }
+        else{
+            rb[i] = Math.max(rb[i+1], height[i])
+        }
+    }
+    
+    for(let i=0; i<height.length; i++){
+        trap += Math.min(lb[i], rb[i]) - height[i]
+    }
+
+    return trap
 };
