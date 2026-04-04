9f21a264a8802705e86cf36e2d0cada2426fe2aa
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-04-04 18:44:31 +0530

    Time: 77 ms (16.60%) | Memory: 63 MB (17.44%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.js
+++ 567-permutation-in-string/permutation-in-string.js
@@ -4,43 +4,29 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-    let m = s1.length, n = s2.length
-    if(m > n) return false
+    if(s2.length < s1.length) return false
+    let freq1 = new Array(26).fill(0), freq2 = new Array(26).fill(0)
 
-    let f1 = new Array(26).fill(0)
-    let f2 = new Array(26).fill(0)
-
-    for(let ch of s1){
-        let index = ch.charCodeAt(0) - 97
-        f1[index]++
+    for(let i=0; i<s1.length; i++){
+        let index1 = s1[i].charCodeAt(0) - 'a'.charCodeAt(0)
+        let index2 = s2[i].charCodeAt(0) - 'a'.charCodeAt(0)
+        freq1[index1]++
+        freq2[index2]++
     }
-    for(let i=0; i<m; i++){
-        let ch = s2[i]
-        let index = ch.charCodeAt(0) - 97
-        f2[index]++
-    }
+    if(freq1.toString() == freq2.toString()) return true
+    let l = 0
 
-    function matches(){
-        let flag = true
-        for(let i=0; i<26; i++){
-            if(f1[i] != f2[i]){
-                flag = false
-                break
-            }
-        }
-        if(flag) return true
-    }
-    if(matches()) return true
+    for(let r = s1.length; r<s2.length; r++){
+        let indexr = s2[r].charCodeAt(0) - 'a'.charCodeAt(0)
+        let indexl = s2[l++].charCodeAt(0) - 'a'.charCodeAt(0)
 
-    for(let i=m; i<n; i++){
-        let prev = s2[i-m], cur = s2[i]
+        freq2[indexr]++
+        freq2[indexl]--
 
-        f2[prev.charCodeAt(0)-97]--
-        f2[cur.charCodeAt(0)-97]++
+        if(freq1.toString() == freq2.toString()) return true
 
-        if(matches()) return true
     }
 
     return false
-    
+
 };
