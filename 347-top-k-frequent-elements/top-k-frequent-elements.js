bb80a0d90e0ddf0d45a8a0e50b07cf623c68396e
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-04-05 16:26:53 +0530

    Time: 9 ms (82.83%) | Memory: 58.7 MB (80.14%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -4,33 +4,25 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    // we can use map for to store their frequencis and freq as key 
-    // sort the map based on frequecy, traverse the map get the k values
-
-
-    let hm = new Map()
+    let hashmap = new Map()
 
     for(let num of nums){
-        if(hm.has(num)){
-            hm.set(num, hm.get(num) + 1) 
-        }else{
-            hm.set(num, 1)
-        }
+        hashmap.set(num, (hashmap.get(num) || 0) + 1)
     }
 
-    let hm_arr = Array.from(hm)
-    hm_arr.sort((a,b) => 
-        b[1] - a[1]
-    )
+    // now we have the frequemcy hashmap
+
+    // sorting the hashmap
+
+    let hasharray = Array.from(hashmap)
+
+    hasharray.sort((a,b) => b[1] - a[1])
 
     let res = []
+
     for(let i=0; i<k; i++){
-        res.push(hm_arr[i][0])
+        res.push(hasharray[i][0])
     }
 
     return res
-
-
-
-
 };
