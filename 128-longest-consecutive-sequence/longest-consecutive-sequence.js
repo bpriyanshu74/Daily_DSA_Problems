0fe5b1a32b8f2c039f655eadeaeb682fccf78412
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-04-06 00:04:35 +0530

    Time: 41 ms (42.91%) | Memory: 78.9 MB (86.77%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -3,23 +3,20 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    let numset = new Set(nums)
-    let numset_arr = Array.from(numset)
-    let longest = 0
-    for(let num of numset_arr){
-        if(numset.has(num-1)) continue
-        else{
-            let temp = 1
-            while(numset.has(num+1)){
-                temp++
-                num += 1
-            }
-            longest = Math.max(temp, longest)
-        }
+  let numset = new Set(nums)
+  let numsarr = Array.from(numset)
+    let maxl = 0
+  for(let num of numsarr){
+    if(numset.has(num-1)) continue
+
+    let len = 1
+    while(numset.has(num+1)){
+        len++
+        num += 1
     }
 
-    return longest
+    maxl = Math.max(len, maxl)
+  }  
 
-    
-    
+  return maxl
 };
