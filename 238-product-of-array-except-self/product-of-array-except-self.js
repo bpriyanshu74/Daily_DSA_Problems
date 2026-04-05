c22a29deb1428818a3a3c32bf9817d8e171ac20e
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-04-05 17:04:57 +0530

    Time: 14 ms (29.03%) | Memory: 69.8 MB (69.14%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,25 +3,23 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let prefix = new Array(nums.length).fill(0), suffix = new Array(nums.length).fill(0)
+  let prefix = new Array(nums.length).fill(1)
+  let suffix = new Array(nums.length).fill(1)
 
-    for(let i=0; i<nums.length; i++){
-        if(i == 0) prefix[i] = 1
-        else{
-            prefix[i] = prefix[i-1] * nums[i-1]
-        }
-    }
+  for(let i=1; i<nums.length; i++){
+    prefix[i] = prefix[i-1] * nums[i-1]
+  }
 
-    for(let i=nums.length-1; i >= 0; i--){
-        if(i == nums.length-1) suffix[i] = 1
-        else{
-            suffix[i] = suffix[i+1] * nums[i+1]
-        }
-    }
+  for(let i=nums.length-2; i>= 0; i--){
+    suffix[i] = suffix[i+1] * nums[i+1]
+  }
 
-    for(let i=0; i<nums.length; i++){
-        prefix[i] *= suffix[i]
-    }
+  for(let i =0; i<nums.length; i++){
+    nums[i] = prefix[i] * suffix[i]
+  }
 
-    return prefix
+  return nums
+
+
+    
 };
