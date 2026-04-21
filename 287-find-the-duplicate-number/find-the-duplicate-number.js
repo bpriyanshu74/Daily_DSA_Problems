abab5122019ce40b0beee5baa4c8453aae14f50f
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-04-21 22:27:06 +0530

    Time: 5 ms (91.82%) | Memory: 68.1 MB (39.01%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -3,18 +3,14 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    let slow = nums[0], fast = nums[0]
+    // here we can use index array
 
-    do{
-        slow = nums[slow]
-        fast = nums[nums[fast]]
-    }while(slow != fast)
+    let temp = new Array(nums.length).fill(0)
 
-    slow = nums[0]
-    while(slow != fast){
-        slow = nums[slow]
-        fast = nums[fast]
+    for(let num of nums){
+        if(temp[num] == 1) return num
+
+        temp[num] = 1
     }
-
-    return slow
+    
 };
