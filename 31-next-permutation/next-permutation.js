4a2bda6537468511b7b5cc56ced943e0a2926a40
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-04-21 10:53:25 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (64.52%) - LeetSync

--- 31-next-permutation/next-permutation.js
+++ 31-next-permutation/next-permutation.js
@@ -3,6 +3,8 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function(nums) {
+    // first we need to find the index where nums[i] < nums[i+1]
+
     let n = nums.length, i = n-2
 
     while(i >= 0 && nums[i] >= nums[i+1]){
@@ -11,16 +13,24 @@ var nextPermutation = function(nums) {
 
     if(i >= 0){
         let j = n-1
-        while(nums[j] <= nums[i]){
+
+        while(j>= 0 && nums[j] <= nums[i]){
             j--
         }
-        [nums[j], nums[i]] = [nums[i], nums[j]]
+        // swapping
+        [nums[i], nums[j]] = [nums[j], nums[i]]
     }
-    let l = i+1, r =n-1
-    while(l < r){
-        [nums[l], nums[r]] = [nums[r], nums[l]]
-        l++
-        r--
+
+    // now reversing from i+1 to n-1
+
+    let p = i+1, q = n-1
+    while(p < q){
+        [nums[p], nums[q]] = [nums[q], nums[p]]
+        p++
+        q--
     }
+
+    
+
     
 };
