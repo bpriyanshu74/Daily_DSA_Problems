332619498f7f2500a3c230d5b32c8c3b53013955
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-04-21 11:38:51 +0530

    Time: 3 ms (60.91%) | Memory: 65.5 MB (38.33%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,37 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    if(prices.length <= 1) return 0
-
-    let profit = 0
-
-    // for(let i=0; i<prices.length-1; i++){
-    //     for(let j=i+1; j<prices.length; j++){
-    //         profit = Math.max(profit, prices[j] - prices[i])
-    //     }
-    // }
-
-    // optimise the n2 time complexity
-
-    let buy = new Array(prices.length).fill(0)
-    let sell = new Array(prices.length).fill(0)
+    let profit = 0, minprice = Infinity
 
     for(let i=0; i<prices.length; i++){
-        if(i == 0) buy[i] = prices[i]
-        else{
-            buy[i] = Math.min(prices[i], buy[i-1])
-        }
+        minprice = Math.min(prices[i], minprice)
+
+        profit = Math.max(profit, prices[i] - minprice)
     }
 
-    for(let i=prices.length-1; i>= 0; i--){
-        if(i == prices.length-1) sell[i] = prices[i]
-        else{
-            sell[i] = Math.max(prices[i], sell[i+1])
-        }
-    }
+    return profit 
 
-    for(let i=0; i<prices.length; i++){
-        profit = Math.max(profit, sell[i] - buy[i])
-    }
-    return profit
+
 };
