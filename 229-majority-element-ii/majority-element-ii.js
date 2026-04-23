e2a78d89d20d08ec8d38f441885c2d9100677d5e
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-04-23 12:38:04 +0530

    Time: 12 ms (24.38%) | Memory: 61.3 MB (31.16%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.js
+++ 229-majority-element-ii/majority-element-ii.js
@@ -3,16 +3,38 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    let hashmap = new Map()
-    let ans = new Set()
+    let c1 = null, c2 = null
+
+    let ct1 = 0, ct2 = 0
 
     for(let num of nums){
-        hashmap.set(num, (hashmap.get(num) || 0) + 1)
+        if(c1 == num) ct1++
+        else if(c2 == num) ct2++
+        else if(ct1 == 0) {c1 = num
+        ct1 = 1}
 
-        if(hashmap.get(num) > Math.floor(nums.length/3)){
-            ans.add(num)
+        else if(ct2 == 0) {c2 = num
+        ct2 = 1}
+        else{
+            ct1--
+            ct2--
         }
     }
+    // verify the numbers
+    ct1 = 0, ct2 = 0
 
-    return Array.from(ans)
+    for(let num of nums){
+        if(num == c1) ct1++
+        else if(num == c2) ct2++
+    }
+
+    let res = []
+    let n = nums.length
+
+    if(ct1 > Math.floor(n/3)){
+        res.push(c1)
+    }
+    if (ct2 > Math.floor(n / 3)) res.push(c2);
+
+    return res
 };
