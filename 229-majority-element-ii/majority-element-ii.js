d791683e1983d8794fa7cab00a387ee3173715d5
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-04-23 12:22:20 +0530

    Time: 8 ms (47.65%) | Memory: 61.3 MB (29.78%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.js
+++ 229-majority-element-ii/majority-element-ii.js
@@ -3,35 +3,16 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    let ele1 = null, ele2 = null, count1 = 0, count2 = 0
+    let hashmap = new Map()
+    let ans = new Set()
 
-    for(let i=0; i<nums.length; i++){
-        if(count1 == 0 && ele2 != nums[i]){
-            ele1 = nums[i]
-            count1 = 1
-        }
-        else if(count2 == 0 && ele1 != nums[i]){
-            ele2 = nums[i]
-            count2 = 1
-        }
-        else if(ele1 == nums[i]) count1++
-        else if(ele2 == nums[i]) count2++
-        else{
-            count1--
-            count2--
+    for(let num of nums){
+        hashmap.set(num, (hashmap.get(num) || 0) + 1)
+
+        if(hashmap.get(num) > Math.floor(nums.length/3)){
+            ans.add(num)
         }
     }
 
-    let res = []
-    let mini = Math.floor(nums.length/3) 
-    let cnt1 = 0, cnt2 = 0
-    for(let i=0; i< nums.length; i++){
-        if(nums[i] == ele1) cnt1++
-        if(nums[i] == ele2) cnt2++
-    }
-
-    if(cnt1 > mini) res.push(ele1)
-    if(cnt2 > mini) res.push(ele2)
-
-    return res
+    return Array.from(ans)
 };
