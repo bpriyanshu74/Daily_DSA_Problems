16325c42b782ceb4bde93490880a2cbeebdaa4d4
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-05-02 11:13:08 +0530

    Time: 1 ms (58.02%) | Memory: 53.1 MB (86.73%) - LeetSync

--- 62-unique-paths/unique-paths.js
+++ 62-unique-paths/unique-paths.js
@@ -4,19 +4,16 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    let dp = Array.from({length: m}, () => new Array(n).fill(0))
-
-    for(let j=0; j<n; j++){
-        dp[0][j] = 1
-    }
-    for(let i=0; i<m; i++){
-        dp[i][0] = 1
-    }
+    if(m == 1 || n == 1) return 1
+    let dp = new Array(m).fill().map(() => new Array(n).fill(1))
+    
     for(let i=1; i<m; i++){
-        for(let j = 1; j<n; j++){
-            dp[i][j] = dp[i-1][j] + dp[i][j-1] 
+        for(let j=1; j<n; j++){
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
         }
     }
-    return dp[m-1][n-1]
-    
+
+
+    return dp[m-2][n-1] + dp[m-1][n-2]
+
 };
