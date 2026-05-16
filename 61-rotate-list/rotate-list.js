d50cd9c38a0cf4714a2d1c548b3b8e430c8c0be2
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-05-17 01:14:41 +0530

    Time: 0 ms (100.00%) | Memory: 57.8 MB (21.86%) - LeetSync

--- 61-rotate-list/rotate-list.js
+++ 61-rotate-list/rotate-list.js
@@ -11,15 +11,16 @@
  * @return {ListNode}
  */
 var rotateRight = function(head, k) {
-    if( !head) return head
+    if(!head) return head
 
     let len = 0, temp = head
+
     while(temp){
         len++
         temp = temp.next
     }
 
-    k = k % len
+    k %= len
     if(k == 0) return head
 
     temp = head
@@ -27,17 +28,19 @@ var rotateRight = function(head, k) {
     for(let i=1; i<len-k; i++){
         temp = temp.next
     }
+
     let new_head = temp.next
     temp.next = null
 
     let temp1 = new_head
+
     while(temp1.next){
         temp1 = temp1.next
     }
+
     temp1.next = head
 
     return new_head
 
-    
-    
+
 };
