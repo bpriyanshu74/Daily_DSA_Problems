bd6ac275a49a18faa02576036f2e1d029cc73e7d
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2026-06-13 17:24:38 +0530

    Time: 4 ms (50.54%) | Memory: 56 MB (42.04%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -6,13 +6,27 @@ var isValid = function(s) {
     let map = {'(': ')', '{': '}', '[': ']'}, stack = []
 
     for(let ch of s){
-        if(ch in map){
-            stack.push(map[ch])
+        if(stack.length == 0){
+            stack.push(ch)
         }else{
-            if(stack.length && ch == stack[stack.length-1]) stack.pop()
-            else return false
+            if(map[ch] != undefined){
+                // opening
+                stack.push(ch)
+            }else{
+                // closing
+                if(stack.length > 0){
+                    if(ch == map[stack[stack.length-1]]){
+                        stack.pop()
+                    }else{
+                        return false
+                    }
+                }else{
+                    // closing comes first breaking the valid rule
+                    return false
+                }
+            }
         }
     }
 
-    return stack.length == 0
+    return stack.length == 0 ? true : false
 };
